883030a02a5c435d17f3dfc73d6bd682df101e65
William Fiset <fisetwill@gmail.com>
2017-03-26 19:08:56 -0300

    Added algorithms

---
+++ TernarySearch.java
@@ -0,0 +1,44 @@
+/**
+ * Similar to binary search, except it works on a function which
+ * decreases and then increases. This method returns the input value
+ * corresponding with the minimum output value of the function. 
+ * Time Complexity: O(log(high - low)). NOTE: To work with a function which 
+ * increases and then decreases, simply negate your function.
+ *
+ * @author Micah Stairs
+ **/
+
+public class TernarySearch {
+
+  // Define a very small epsilon value to compare double values
+  static final double EPS = 0.000000001;
+
+  // Define your own function on whatever you're attempting to ternary
+  // search. Remember that your function must be a continuous unimodal
+  // function, meaning a function which decreases then increases (U shape)
+  // or a function which increases and then decreases.
+  static double f(double x) { return x*x + 3*x + 5; }
+
+  static double ternarySearch(double low, double high) {
+    Double best = null;
+    while (true) {
+      double mid1 = (2*low + high) / 3, mid2 = (low + 2*high) / 3;
+      double res1 = f(mid1), res2 = f(mid2);
+      if (res1 > res2) low = mid1;
+      else high = mid2;
+      if (best != null && Math.abs(best - mid1) < EPS) break;
+      best = mid1;
+    }
+    return best;
+  }
+
+  public static void main(String[] args) {
+
+    // Search for the lowest point on the function x^2 + 3x + 5 
+    // using a ternary search on the interval [-100, +100]
+    double root = ternarySearch(-100.0, +100.0);
+    System.out.printf("%.4f\n", root);
+
+  }
+
+}
