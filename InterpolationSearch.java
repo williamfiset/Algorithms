1262f3ecb2099c9c2a582b4002dd9ded965d4b31
William Fiset <fisetwill@gmail.com>
2017-03-26 18:52:08 -0300

    Added algorithms

--- InterpolationSearch.java
+++ InterpolationSearch.java
@@ -1,8 +1,12 @@
 
 public class InterpolationSearch {
 
-  // nums - an ordered list containing uniformly distributed values
-  // val  - the value we're looking for
+  /**
+   * A fast alternative to a binary search when the elements are uniformly distributed.
+   * This algorithm runs in a time complexity of ~O(log(log(n))).
+   * @param nums - an ordered list containing uniformly distributed values.
+   * @param val  - the value we're looking for in 'nums'
+   **/
   static int interpolationSearch(int[] nums, int val) {
     int lo = 0, mid = 0, hi = nums.length - 1;
     while (nums[lo] <= val && nums[hi] >= val) {
@@ -18,4 +22,17 @@ public class InterpolationSearch {
     return -1;
   }
   
+  public static void main(String[] args) {
+    
+    int [] values = { 10, 20, 25, 35, 50, 70, 85, 100, 110, 120, 125 };
+
+    // Since 25 exists in the values array the interpolation search
+    // returns that it has found 25 at the index 2
+    System.out.println( interpolationSearch(values, 25) );
+
+    // 111 does not exist so we get -1 as an index value
+    System.out.println( interpolationSearch(values, 111) );
+
+  }
+
 }
