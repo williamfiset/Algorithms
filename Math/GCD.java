03c6bdb1eadbb16d824813fb65d842a028260813
william-fiset <fisetwill@gmail.com>
2017-05-04 22:33:41 -0300

    Bug fixes

--- Math/GCD.java
+++ Math/GCD.java
@@ -1,11 +1,16 @@
 public class GCD {
 
   static long gcd(long a, long b) {
-    return b == 0 ? a : gcd(b, a % b);
+    return b == 0 ? (a < 0 ? -a : a) : gcd(b, a % b);
   }
 
   public static void main(String[] args) {
-    System.out.println(gcd(12,18)); // 6
+
+    System.out.println(gcd(12, 18));  // 6
+    System.out.println(gcd(-12, 18)); // 6
+    System.out.println(gcd(12, -18)); // 6
+    System.out.println(gcd(-12,-18)); // 6
+
   }
 
 }
--- Math/ModularInverse.java
+++ Math/ModularInverse.java
@@ -6,36 +6,48 @@ public class ModularInverse {
   // and can be used for several things such as finding modular inverses and 
   // solutions to linear Diophantine equations.    
   static long[] egcd(long a, long b) {
-    if (b == 0)
-      return new long[] { a, 1, 0 };
-    else {
-      long[] ret = egcd(b, a % b);
-      long tmp = ret[1] - ret[2] * (a / b);
-      ret[1] = ret[2];
-      ret[2] = tmp;
-      return ret;
-    }
+    if (b == 0) return new long[] { a, 1L, 0L };
+    long[] v = egcd(b, a % b);
+    long tmp = v[1] - v[2] * (a / b);
+    v[1] = v[2]; v[2] = tmp;
+    return v;
   }
 
   // Returns the modular inverse of 'a' mod 'm' if it exists.
   // Make sure m > 0 and 'a' & 'm' are relatively prime.
   static Long modInv(long a, long m) {
 
-    long[] ret = egcd(a, m);
-    long gcf = ret[0];
-    long x = ret[1];
-    if (gcf == 1)
-      return (x % m) + m;
-    // throw new ArithmeticException("m <= 0, or 'a' has no multiplicative inverse mod m");
-    return null;
+    if (m <= 0) throw new ArithmeticException("mod must be > 0");
 
-  }  
+    // Avoid a being negative
+    a = ((a % m) + m) % m;
+
+    long[] v = egcd(a, m);
+    long gcd = v[0];
+    long x   = v[1];
+
+    if (gcd != 1) return null;
+    return ((x + m) % m) % m;
+
+  }
 
   public static void main(String[] args) {
     
     // Prints 3 since 2*3 mod 5 = 1
     System.out.println(modInv(2, 5));
 
+    // Prints null because there is no
+    // modular inverse such that 4*x mod 18 = 1  
+    System.out.println(modInv(4, 18));
+
   }
 
 }
+
+
+
+
+
+
+
+
