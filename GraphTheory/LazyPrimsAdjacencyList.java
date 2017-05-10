86b16f50b5521897684f7c9fd2e4ccab163cbed4
william-fiset <fisetwill@gmail.com>
2017-05-10 20:16:23 -0300

    Added lazy prims alg

---
+++ GraphTheory/LazyPrimsAdjacencyList.java
@@ -0,0 +1,141 @@
+/**
+ * An implementation of the lazy implementation of Prims algorithm
+ * which upon visiting a new node adds all the edges to the min priority
+ * queue and also removes already seen edges when polling.
+ *
+ * @author William Fiset, william.alexandre.fiset@gmail.com
+ **/
+
+import java.util.*;
+
+class Edge implements Comparable <Edge> {
+  int from, to, cost;
+  public Edge(int from, int to, int cost) {
+    this.from = from;
+    this.to = to;
+    this.cost = cost;
+  }
+  @Override public int compareTo(Edge other) {
+    return cost - other.cost;
+  }
+}
+
+public class LazyPrimsAdjacencyList {
+  
+  public static Long prims(Map <Integer, List<Edge>> graph, int n) {
+    
+    if (graph == null) return null;
+    
+    long sum = 0, nodesVisited = 1;
+    boolean[] visited = new boolean[n];
+    PriorityQueue <Edge> frontier = new PriorityQueue<>();
+    
+    // Add initial set of edges to the frontier
+    List <Edge> edges = graph.get(0);
+    if (edges == null || edges.size() == 0) return null;
+    for(Edge edge : edges) frontier.offer(edge);
+    visited[0] = true;
+
+    // Loop while the MST is not complete
+    while ( nodesVisited != n && !frontier.isEmpty() ) {
+      Edge edge = frontier.poll();
+      if (!visited[edge.to]) {
+        
+        nodesVisited++;
+        sum += edge.cost;
+        
+        // Add edges to nodes we have not yet visited
+        edges = graph.get(edge.to);
+        if (edges == null || edges.size() == 0) return null;
+        for (Edge e : edges) 
+          if (!visited[e.to])
+            frontier.offer(e);
+
+        visited[edge.to] = true;
+
+      }
+    }
+    
+    // Check if MST spans entire graph
+    if (nodesVisited == n) return sum;
+    return null;
+
+  }
+
+  public static void main(String[] args) {
+    
+    // Contains tuples of (from, to, weight)
+    int[][] edges = {
+      
+      {0, 1, 1},
+      {0, 3, 4},
+      {0, 4, 5},
+
+      {1, 3, 2},
+      {1, 2, 1},
+
+      {2, 3, 5},
+      {2, 5, 7},
+
+      {3, 4, 2},
+      {3, 6, 2},
+      {3, 5, 11},
+
+      {4, 7, 4},
+
+      {5, 6, 1},
+      {5, 8, 4},
+
+      {6, 7, 4},
+      {6, 8, 6},
+
+      {7, 8, 1},
+      {7, 9, 2},
+
+      {8, 9, 0}
+
+    };
+
+    final int NUM_NODES = 11;
+
+    // Setup graph as adjacency list
+    Map <Integer, List<Edge>> graph = new HashMap<>();
+    for (int i = 0; i < NUM_NODES; i++) graph.put(i, new ArrayList<>());
+    for ( int[] tuple : edges ) {
+      int from = tuple[0];
+      int to = tuple[1];
+      int cost = tuple[2];
+      Edge edge = new Edge(from, to, cost);
+      Edge revEdge = new Edge(to, from, cost);
+      graph.get(from).add(edge);
+      graph.get(to).add(revEdge);
+    }
+
+    Long mstCost = prims(graph, NUM_NODES);
+    System.out.println("MST cost: " + mstCost);
+
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
