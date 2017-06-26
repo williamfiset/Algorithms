054c0fe83c5078f04ed6af9926a9c76a5a6a0420
william-fiset <fisetwill@gmail.com>
2017-06-26 16:35:30 -0300

    Added mergesort

--- SortingAlgorithms/Mergesort.java
+++ SortingAlgorithms/Mergesort.java
@@ -11,35 +11,36 @@ public class Mergesort {
   
   public static int[] mergesort( int[] ar ) {
     
+    // Base case is when a single element (which is already sorted)
     int n = ar.length;
     if (n == 1) return ar;
     
+    // Split array into two parts and recursively sort them
     int[] left  = mergesort(Arrays.copyOfRange(ar, 0, n/2));
     int[] right = mergesort(Arrays.copyOfRange(ar, n/2, n));
     
+    // Combine the two arrays into one larger array
     return merge(left, right);
     
   }
   
-  // Merge two sorted arrays into a larger array
+  // Merge two sorted arrays into a larger sorted array
   private static int[] merge(int [] ar1, int[] ar2) {
     
-    int n1 = ar1.length;
-    int n2 = ar2.length;
-    int n = n1 + n2, i1 = 0, i2 = 0, k = 0;
+    int n1 = ar1.length, n2 = ar2.length;
+    int n = n1 + n2, i1 = 0, i2 = 0;
     int[] ar = new int[n];
     
-    while(i1 < n1 || i2 < n2) {
-      
+    for(int i = 0; i < n; i++) {
       if (i1 == n1) {
-        ar[k++] = ar2[i2++];
+        ar[i] = ar2[i2++];
       } else if (i2 == n2) {
-        ar[k++] = ar1[i1++];
+        ar[i] = ar1[i1++];
       } else {
         if (ar1[i1] < ar2[i2]) {
-          ar[k++] = ar1[i1++];
+          ar[i] = ar1[i1++];
         } else {
-          ar[k++] = ar2[i2++];
+          ar[i] = ar2[i2++];
         }
       }
     }
