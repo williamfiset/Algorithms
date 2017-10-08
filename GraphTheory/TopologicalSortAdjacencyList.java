d1edf4f8ad8cf6f6ca94775b3419eee43166b3f3
william-fiset <fisetwill@gmail.com>
2017-10-07 20:54:34 -0700

    Topsort work

--- GraphTheory/TopologicalSortAdjacencyList.java
+++ GraphTheory/TopologicalSortAdjacencyList.java
@@ -26,17 +26,16 @@ public class TopologicalSortAdjacencyList {
   // us the topological ordering we want. Instead of maintaining a stack 
   // of the nodes we see we simply place them inside the ordering array
   // in reverse order for simplicity.
-  private static int topologicalSortDFS(int i, int at, boolean[] visited, int[] ordering, Map<Integer, List<Edge>> adjList) {
+  private static int dfs(int i, int at, boolean[] visited, int[] ordering, Map<Integer, List<Edge>> graph) {
 
-    if (visited[at]) return i;
     visited[at] = true;
 
-    List<Edge> edges = adjList.get(at);
+    List<Edge> edges = graph.get(at);
 
     if (edges != null)
-      for (Edge edge: edges)
+      for (Edge edge : edges)
         if (!visited[edge.to])
-          i = topologicalSortDFS(i, edge.to, visited, ordering, adjList);
+          i = dfs(i, edge.to, visited, ordering, graph);
 
     ordering[i] = at;
     return i-1;
@@ -51,7 +50,7 @@ public class TopologicalSortAdjacencyList {
   // in the adjacency list since you can have singleton nodes with no edges which
   // wouldn't be present in the adjacency list but are still part of the graph!
   //
-  public static int [] topologicalSort(Map<Integer, List<Edge>> adjList, int numNodes) {
+  public static int[] topologicalSort(Map<Integer, List<Edge>> graph, int numNodes) {
 
     int[] ordering = new int[numNodes];
     boolean[] visited = new boolean[numNodes];
@@ -59,7 +58,7 @@ public class TopologicalSortAdjacencyList {
     int i = numNodes - 1;
     for (int at = 0; at < numNodes; at++)
       if (!visited[at])
-        i = topologicalSortDFS(i, at, visited, ordering, adjList);
+        i = dfs(i, at, visited, ordering, graph);
 
     return ordering;
 
@@ -73,9 +72,9 @@ public class TopologicalSortAdjacencyList {
   // in the adjacency list since you can have singleton nodes with no edges which
   // wouldn't be present in the adjacency list but are still part of the graph!
   // 
-  public static Integer[] dagShortestPath(Map <Integer, List<Edge>> adjList, int start, int numNodes) {
+  public static Integer[] dagShortestPath(Map<Integer, List<Edge>> graph, int start, int numNodes) {
 
-    int[] topsort = topologicalSort(adjList, numNodes);
+    int[] topsort = topologicalSort(graph, numNodes);
     Integer[] dist = new Integer[numNodes];
     dist[start] = 0;
 
@@ -83,7 +82,7 @@ public class TopologicalSortAdjacencyList {
 
       int nodeIndex = topsort[i];
       if (dist[nodeIndex] != null) {
-        List<Edge> adjacentEdges = adjList.get(nodeIndex);
+        List<Edge> adjacentEdges = graph.get(nodeIndex);
         if (adjacentEdges != null) {
           for (Edge edge : adjacentEdges) {
 
@@ -107,25 +106,25 @@ public class TopologicalSortAdjacencyList {
     
     // Graph setup    
     final int N = 7;
-    Map<Integer, List<Edge>> adjList = new HashMap<>();
-    for (int i = 0; i < N; i++) adjList.put(i, new ArrayList<>());
-    adjList.get(0).add(new Edge(0,1,3));
-    adjList.get(0).add(new Edge(0,2,2));
-    adjList.get(0).add(new Edge(0,5,3));
-    adjList.get(1).add(new Edge(1,3,1));
-    adjList.get(1).add(new Edge(1,2,6));
-    adjList.get(2).add(new Edge(2,3,1));
-    adjList.get(2).add(new Edge(2,4,10));
-    adjList.get(3).add(new Edge(3,4,5));
-    adjList.get(5).add(new Edge(5,4,7));
+    Map<Integer, List<Edge>> graph = new HashMap<>();
+    for (int i = 0; i < N; i++) graph.put(i, new ArrayList<>());
+    graph.get(0).add(new Edge(0,1,3));
+    graph.get(0).add(new Edge(0,2,2));
+    graph.get(0).add(new Edge(0,5,3));
+    graph.get(1).add(new Edge(1,3,1));
+    graph.get(1).add(new Edge(1,2,6));
+    graph.get(2).add(new Edge(2,3,1));
+    graph.get(2).add(new Edge(2,4,10));
+    graph.get(3).add(new Edge(3,4,5));
+    graph.get(5).add(new Edge(5,4,7));
 
-    int[] ordering = topologicalSort(adjList, N);
+    int[] ordering  = topologicalSort(graph, N);
 
     // // Prints: [6, 0, 5, 1, 2, 3, 4]
     System.out.println(java.util.Arrays.toString(ordering));
 
     // Finds all the shortest paths starting at node 0
-    Integer[] dists = dagShortestPath(adjList, 0, N);
+    Integer[] dists = dagShortestPath(graph, 0, N);
 
     // Find the shortest path from 0 to 4 which is 8.0
     System.out.println(dists[4]);
