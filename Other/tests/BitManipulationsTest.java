9f21d6789118fa3064eb798c43f9ca5af0590b04
william-fiset <fisetwill@gmail.com>
2017-11-17 23:21:55 -0800

    Added a bit manipulation test

--- Other/tests/BitManipulationsTest.java
+++ Other/tests/BitManipulationsTest.java
@@ -17,4 +17,27 @@ public class BitManipulationsTest {
     assertThat(BitManipulations.setBit(0, 5)).isEqualTo(0b100000);
   }
   
+  @Test
+  public void testPowerOfTwo() {
+    assertThat(BitManipulations.isPowerOfTwo(0)).isFalse();
+    assertThat(BitManipulations.isPowerOfTwo(-1)).isFalse();
+    assertThat(BitManipulations.isPowerOfTwo(7)).isFalse();
+    assertThat(BitManipulations.isPowerOfTwo(9)).isFalse();
+    assertThat(BitManipulations.isPowerOfTwo(123456789)).isFalse();
+    
+    assertThat(BitManipulations.isPowerOfTwo(1)).isTrue();
+    assertThat(BitManipulations.isPowerOfTwo(2)).isTrue();
+    assertThat(BitManipulations.isPowerOfTwo(4)).isTrue();
+    assertThat(BitManipulations.isPowerOfTwo(2048)).isTrue();
+    assertThat(BitManipulations.isPowerOfTwo(1 << 20)).isTrue();
+  }
+  
+  @Test
+  public void testClearBit() {
+    assertThat(BitManipulations.clearBit(0b0000, 1)).isEqualTo(0);
+    assertThat(BitManipulations.clearBit(0b0100, 2)).isEqualTo(0);
+    assertThat(BitManipulations.clearBit(0b0001, 0)).isEqualTo(0);
+    assertThat(BitManipulations.clearBit(0b1111, 0)).isEqualTo(14);
+  }
+  
 }
