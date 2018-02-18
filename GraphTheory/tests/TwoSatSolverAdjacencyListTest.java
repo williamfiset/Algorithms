0fefc4cc66576f5c54e40e1c16428d90d75b12c8
william-fiset <fisetwill@gmail.com>
2018-02-17 21:58:36 -0800

    2SAT work

--- GraphTheory/tests/TwoSatSolverAdjacencyListTest.java
+++ GraphTheory/tests/TwoSatSolverAdjacencyListTest.java
@@ -15,14 +15,45 @@ public class TwoSatSolverAdjacencyListTest {
   }
 
   @Test
-  public void testSingleClauseSatisfiableCase() {
+  public void testSimpleSatisfiable() {
     int n = 1;
-    List<List<Integer>> graph = createGraph(n);
-    // Add clause: (p or ~p)
-    TwoSatSolverAdjacencyList.addOrClause(graph, 0, 0 ^ 1);
-    TwoSatSolverAdjacencyList solver = new TwoSatSolverAdjacencyList(graph);
+    List<List<Integer>> g = createGraph(n);
+
+    // Full clause: (p or ~p)
+    TwoSatSolverAdjacencyList.addOrClause(g, 0, 0 ^ 1);
+
+    TwoSatSolverAdjacencyList solver = new TwoSatSolverAdjacencyList(g);
     assertThat(solver.isSatisfiable()).isTrue();
   }
+
+  @Test
+  public void testSimpleImpossible() {
+    int n = 1;
+    List<List<Integer>> g = createGraph(n);
+    
+    // Full clause: (p or p) and (~p or ~p)
+    TwoSatSolverAdjacencyList.addOrClause(g, 0, 0); // (p or p)
+    TwoSatSolverAdjacencyList.addOrClause(g, 0 ^ 1, 0 ^ 1); // (~p or ~p)
+    
+    TwoSatSolverAdjacencyList solver = new TwoSatSolverAdjacencyList(g);
+    assertThat(solver.isSatisfiable()).isFalse();
+  }
+
+  @Test
+  public void testImpossibleFourNodeCycle() {
+    int n = 2;
+    List<List<Integer>> g = createGraph(n);
+
+    // Full clause: (p0 or p1) and (p0 or ~p1) and (~p0 or p1) and (~p0 or ~p1)
+    TwoSatSolverAdjacencyList.addOrClause(g, 2*0, 2*1);         // (p0 or p1)
+    TwoSatSolverAdjacencyList.addOrClause(g, 2*0, 2*1 ^ 1);     // (p0 or ~p1)
+    TwoSatSolverAdjacencyList.addOrClause(g, 2*0 ^ 1, 2*1);     // (~p0 or p1)
+    TwoSatSolverAdjacencyList.addOrClause(g, 2*0 ^ 1, 2*1 ^ 1); // (~p0 or ~p1)
+    System.out.println(g);
+
+    TwoSatSolverAdjacencyList solver = new TwoSatSolverAdjacencyList(g);
+    assertThat(solver.isSatisfiable()).isFalse();
+  }  
 
 }
 
