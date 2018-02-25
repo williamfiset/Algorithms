9f97f11ed9267616358e6671290397665e0f0dab
william-fiset <fisetwill@gmail.com>
2018-02-25 09:45:17 -0800

    fw apsp work

--- GraphTheory/FloydWarshallSolver.java
+++ GraphTheory/FloydWarshallSolver.java
@@ -25,9 +25,8 @@ public class FloydWarshallSolver {
   private Integer[][] next;
 
   /**
-   * As input, this class takes an adjacency matrix with edge 
-   * weights between nodes, where POSITIVE_INFINITY is used 
-   * to indicate that two nodes are not, connected.
+   * As input, this class takes an adjacency matrix with edge weights between nodes, 
+   * where POSITIVE_INFINITY is used to indicate that two nodes are not connected.
    *
    * NOTE: Usually the diagonal of the adjacency matrix is all zeros 
    * (i.e. matrix[i][i] = 0 for all i) since there is typically no cost 
@@ -69,7 +68,6 @@ public class FloydWarshallSolver {
 
   // Executes the Floyd-Warshall algorithm internally.
   public void solve() {
-
     // Compute all pairs shortest paths.
     for (int k = 0; k < n; k++) {
       for (int i = 0; i < n; i++) {
@@ -93,6 +91,7 @@ public class FloydWarshallSolver {
     solved = true;
   }
 
+  // TODO(williamfiset): Remove this test method
   public static double[][] floydWarshallExtraMemory(double[][] m) {
     int v = m.length;
     double[][][] dp = new double[v][v][v];
@@ -121,51 +120,6 @@ public class FloydWarshallSolver {
     FloydWarshallSolver solver = new FloydWarshallSolver(m);
     List<Integer> path = solver.getPath(0, 3);
     System.out.println(path);
-    // double[][] result = FloydWarshall.floydWarshall(m);
-    // for (int i = 0; i < result.length; i++) {
-    //   for (int j = 0; j < result.length; j++) {
-    //     double dist = result[i][j];
-    //     if (dist == Double.NEGATIVE_INFINITY) {
-    //       System.out.printf("Edge %d to %d is part of or reaches into a negative cycle\n", i, j);
-    //     } else if (dist == Double.POSITIVE_INFINITY) {
-    //       System.out.printf("Node %d does not reach node %d\n", i, j);
-    //     } else {
-    //       System.out.printf("The distance from node %d to node %d is %.3f\n", i, j, dist);
-    //     }
-    //   }
-    // }
-
-    // for (double[] r : result) {
-    //   System.out.println(java.util.Arrays.toString(r));
-    // }
-    // double[][] matrix2 = new double[][] {
-    //   {  0,   3,   1,   8, INF},
-    //   {  2,   0,   9,   4, INF},
-    //   {INF, INF,   0, INF,  -2},
-    //   {INF, INF,   1,   0, INF},
-    //   {INF, INF, INF,   0,   0}
-    // };
-    // double[][] matrix2FromSlides = new double[][] {
-    //   {   0,   6, INF,  25,   3},
-    //   {   1,   0,   6,   1,   3},
-    //   { INF,   1,   0,   2,   3},
-    //   {   4,   4,   4,   0, INF},
-    //   {   4,   3,   5, INF,   0}
-    // };
-    // FloydWarshall.floydWarshall(matrix2FromSlides);
-    // for (double[] r : matrix2FromSlides) {
-    //   System.out.println(Arrays.toString(r));
-    // }
-    // System.out.println();
-    // double[][] matrix2FromSlides2 = new double[][] {
-    //   {   0,   6, INF,  25,   3},
-    //   {   1,   0,   6,   1,   3},
-    //   { INF,   1,   0,   2,   3},
-    //   {   4,   4,   4,   0, INF},
-    //   {   4,   3,   5, INF,   0}
-    // };
-    // FloydWarshall.floydWarshallExtraMemory(matrix2FromSlides2);
-      
   }
 
 }
