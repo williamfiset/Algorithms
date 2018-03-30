94a3c4e717c402acd681f4579824493a8ef8c8f6
william-fiset <fisetwill@gmail.com>
2018-03-29 19:03:54 -0700

    Connected Components fix

--- com/williamfiset/algorithms/graphtheory/ConnectedComponentsDfsSolverAdjacencyList.java
+++ com/williamfiset/algorithms/graphtheory/ConnectedComponentsDfsSolverAdjacencyList.java
@@ -48,7 +48,6 @@ public class ConnectedComponentsDfsSolverAdjacencyList {
         componentCount++;
         dfs(i);
       }
-      components[i] = componentCount;
     }
 
     solved = true;
@@ -56,6 +55,7 @@ public class ConnectedComponentsDfsSolverAdjacencyList {
 
   private void dfs(int at) {
     visited[at] = true;
+    components[at] = componentCount;
     for (int to : graph.get(at))
       if (!visited[to])
         dfs(to);
@@ -76,16 +76,18 @@ public class ConnectedComponentsDfsSolverAdjacencyList {
 
   public static void main(String[] args) {
 
-    final int n = 7;
+    final int n = 11;
     List<List<Integer>> graph = createGraph(n);
 
-    // Setup a graph with four connected components
-    // namely: {0,1,2}, {3,4}, {5}, {6}
+    // Setup a graph with five connected components:
+    // {0,1,7}, {2,5}, {4,8}, {3,6,9}, {10}
     addUndirectedEdge(graph, 0, 1);
-    addUndirectedEdge(graph, 1, 2);
-    addUndirectedEdge(graph, 2, 0);
-    addUndirectedEdge(graph, 3, 4);
-    addUndirectedEdge(graph, 5, 5); // Self loop
+    addUndirectedEdge(graph, 1, 7);
+    addUndirectedEdge(graph, 7, 0);
+    addUndirectedEdge(graph, 2, 5);
+    addUndirectedEdge(graph, 4, 8);
+    addUndirectedEdge(graph, 3, 6);
+    addUndirectedEdge(graph, 6, 9);
 
     ConnectedComponentsDfsSolverAdjacencyList solver;
     solver = new ConnectedComponentsDfsSolverAdjacencyList(graph);
@@ -97,14 +99,18 @@ public class ConnectedComponentsDfsSolverAdjacencyList {
       System.out.printf("Node %d is part of component %d\n", i, components[i]);
 
     // Prints:
-    // Number of components: 4
+    // Number of components: 5
     // Node 0 is part of component 1
     // Node 1 is part of component 1
-    // Node 2 is part of component 1
-    // Node 3 is part of component 2
-    // Node 4 is part of component 2
-    // Node 5 is part of component 3
-    // Node 6 is part of component 4
+    // Node 2 is part of component 2
+    // Node 3 is part of component 3
+    // Node 4 is part of component 4
+    // Node 5 is part of component 2
+    // Node 6 is part of component 3
+    // Node 7 is part of component 1
+    // Node 8 is part of component 4
+    // Node 9 is part of component 3
+    // Node 10 is part of component 5
   }
 
 }
