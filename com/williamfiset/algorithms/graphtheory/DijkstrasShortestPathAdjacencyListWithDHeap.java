35da9cdd43aa85cbaec555941a4ce605d4a67d65
william-fiset <fisetwill@gmail.com>
2018-06-28 21:01:54 -0700

    Dijkstra tweaks

--- com/williamfiset/algorithms/graphtheory/DijkstrasShortestPathAdjacencyListWithDHeap.java
+++ com/williamfiset/algorithms/graphtheory/DijkstrasShortestPathAdjacencyListWithDHeap.java
@@ -49,6 +49,12 @@ public class DijkstrasShortestPathAdjacencyListWithDHeap {
     createEmptyGraph();
   }
 
+  // Construct an empty graph with n nodes including the source and sink nodes.
+  private void createEmptyGraph() {
+    graph = new ArrayList<>(n);
+    for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
+  }
+
   /**
    * Adds a directed edge to the graph.
    *
@@ -67,27 +73,6 @@ public class DijkstrasShortestPathAdjacencyListWithDHeap {
    */
   public List<List<Edge>> getGraph() {
     return graph;
-  }
-
-  /**
-   * Reconstructs the shortest path (of nodes) from 'start' to 'end' inclusive.
-   *
-   * @return An array of nodes indexes of the shortest path from 'start'
-   * to 'end'. If 'start' and 'end' are not connected then an empty array
-   * is returned.
-   */
-  public List<Integer> reconstructPath(int start, int end) {
-    if (end < 0 || end >= n)
-      throw new IllegalArgumentException("Invalid node index");
-    if (start < 0 || start >= n)
-      throw new IllegalArgumentException("Invalid node index");
-    List<Integer> path = new ArrayList<>();
-    double dist = dijkstra(start, end);
-    if (dist == Double.POSITIVE_INFINITY) return path;
-    for(Integer at = end; at != null; at = prev[at])
-      path.add(at);
-    Collections.reverse(path);
-    return path;
   }
 
   // Run Dijkstra's algorithm on a directed graph to find the shortest path
@@ -149,10 +134,25 @@ public class DijkstrasShortestPathAdjacencyListWithDHeap {
     return Double.POSITIVE_INFINITY;
   }
 
-  // Construct an empty graph with n nodes including the source and sink nodes.
-  private void createEmptyGraph() {
-    graph = new ArrayList<>(n);
-    for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
+  /**
+   * Reconstructs the shortest path (of nodes) from 'start' to 'end' inclusive.
+   *
+   * @return An array of node indexes of the shortest path from 'start'
+   * to 'end'. If 'start' and 'end' are not connected then an empty array
+   * is returned.
+   */
+  public List<Integer> reconstructPath(int start, int end) {
+    if (end < 0 || end >= n)
+      throw new IllegalArgumentException("Invalid node index");
+    if (start < 0 || start >= n)
+      throw new IllegalArgumentException("Invalid node index");
+    List<Integer> path = new ArrayList<>();
+    double dist = dijkstra(start, end);
+    if (dist == Double.POSITIVE_INFINITY) return path;
+    for(Integer at = end; at != null; at = prev[at])
+      path.add(at);
+    Collections.reverse(path);
+    return path;
   }
 
 }
