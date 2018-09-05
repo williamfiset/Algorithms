f04038cbbfb3686343fd8d7b358d5e85f29507a1
william-fiset <fisetwill@gmail.com>
2018-09-04 23:18:45 -0700

    Mice and owls

--- com/williamfiset/algorithms/graphtheory/networkflow/examples/MiceAndOwls.java
+++ com/williamfiset/algorithms/graphtheory/networkflow/examples/MiceAndOwls.java
@@ -1,3 +1,15 @@
+/**
+ * Build:
+ * $ javac com/williamfiset/algorithms/graphtheory/networkflow/examples/MiceAndOwls.java
+ *
+ * Run:
+ * $ java com/williamfiset/algorithms/graphtheory/networkflow/examples/MiceAndOwls
+ */
+
+package com.williamfiset.algorithms.graphtheory.networkflow.examples;
+
+import static java.lang.Math.min;
+
 import java.awt.geom.*;
 import java.util.*;
 
@@ -38,7 +50,7 @@ public class MiceAndOwls {
     final int T = N - 2;
     
     NetworkFlowSolverBase solver;
-    solver = new FordFulkersonDfsSolverAdjacencyList(N, S, T);
+    solver = new FordFulkersonDfsSolver(N, S, T);
 
     // Source to mice
     for (int i = 0; i < M; i++) {
@@ -61,167 +73,189 @@ public class MiceAndOwls {
       solver.addEdge(M+i, T, holes[i].capacity);
     }
 
+    // Output: "Number of safe mice: 4"
     System.out.println("Number of safe mice: " + solver.getMaxFlow());
 
   }
 
-}
+    /* Network flow solver code */
 
-class Edge {
-  public int from, to;
-  public Edge residual;
-  public long flow, capacity;
-  final public long originalCapacity;
-  
-  public Edge(int from, int to, long capacity) {
-    this.from = from;
-    this.to = to;
-    this.originalCapacity = this.capacity = capacity;
-  }
+  private static class Edge {
+    public int from, to;
+    public Edge residual;
+    public long flow;
+    public final long capacity;
+    
+    public Edge(int from, int to, long capacity) {
+      this.from = from;
+      this.to = to;
+      this.capacity = capacity;
+    }
 
-  public String toString(int s, int t) {
-    String u = (from == s) ? "s" : ((from == t) ? "t" : String.valueOf(from));
-    String v = (to == s) ? "s" : ((to == t) ? "t" : String.valueOf(to));
-    return String.format("Edge %s -> %s = %d/%d", u, v, flow, originalCapacity);
-  }
-}
+    public boolean isResidual() {
+      return capacity == 0;
+    }
 
-abstract class NetworkFlowSolverBase {
+    public long remainingCapacity() {
+      return capacity - flow;
+    }
 
-  // To avoid overflow, set infinity to a value less than Long.MAX_VALUE;
-  protected static final long INF = Long.MAX_VALUE / 2;
+    public void augment(long bottleNeck) {
+      flow += bottleNeck;
+      residual.flow -= bottleNeck;
+    }
 
-  // Inputs: n = number of nodes, s = source, t = sink
-  protected final int n, s, t;
-
-  // 'visited' and 'visitedToken' are variables used in graph sub-routines to 
-  // track whether a node has been visited or not. In particular, node 'i' was 
-  // recently visited if visited[i] == visitedToken is true. This is handy 
-  // because to mark all nodes as unvisited simply increment the visitedToken.
-  protected int visitedToken = 1;
-  protected int[] visited;
-
-  // Indicates whether the network flow algorithm has ran. We should not need to
-  // run the solver multiple times, because it always yields the same result.
-  protected boolean solved;
-
-  protected long maxFlow;
-
-  protected List<Edge>[] graph;
-
-  /**
-   * Creates an instance of a flow network solver. Use the {@link #addEdge(int, int, int)}
-   * method to add edges to the graph.
-   *
-   * @param n - The number of nodes in the graph including source and sink nodes.
-   * @param s - The index of the source node, 0 <= s < n
-   * @param t - The index of the sink node, 0 <= t < n
-   */
-  public NetworkFlowSolverBase(int n, int s, int t) {
-    this.n = n; this.s = s; this.t = t; 
-    initializeGraph();
-    visited = new int[n];
-  }
-
-  // Construct an empty graph with n nodes including the source and sink nodes.
-  private void initializeGraph() {
-    graph = new List[n];
-    for (int i = 0; i < n; i++)
-      graph[i] = new ArrayList<Edge>();
-  }
-
-  /**
-   * Adds a directed edge (and residual edge) to the flow graph.
-   *
-   * @param from     - The index of the node the directed edge starts at.
-   * @param to       - The index of the node the directed edge end at.
-   * @param capacity - The capacity of the edge.
-   */
-  public void addEdge(int from, int to, long capacity) {
-    Edge e1 = new Edge(from, to, capacity);
-    Edge e2 = new Edge(to, from, 0);
-    e1.residual = e2;
-    e2.residual = e1;
-    graph[from].add(e1);
-    graph[to].add(e2);
-  }
-
-  /**
-   * Returns the graph after the solver has been executed. This allow you to
-   * inspect the {@link Edge#flow} compared to the {@link Edge#capacity} in 
-   * each edge. This is useful if you want to figure out which edges were 
-   * used during the max flow.
-   */
-  public List<Edge>[] getGraph() {
-    execute();
-    return graph;
-  }
-
-  // Returns the maximum flow from the source to the sink.
-  public long getMaxFlow() {
-    execute();
-    return maxFlow;
-  }
-
-  // Wrapper method that ensures we only call solve() once
-  private void execute() {
-    if (solved) return; 
-    solve();
-    solved = true;
-  }
-
-  // Method to implement which solves the network flow problem.
-  public abstract void solve();
-
-}
-
-class FordFulkersonDfsSolverAdjacencyList extends NetworkFlowSolverBase {
-
-  /**
-   * Creates an instance of a flow network solver. Use the {@link #addEdge(int, int, int)}
-   * method to add edges to the graph.
-   *
-   * @param n - The number of nodes in the graph including source and sink nodes.
-   * @param s - The index of the source node, 0 <= s < n
-   * @param t - The index of the sink node, 0 <= t < n
-   */
-  public FordFulkersonDfsSolverAdjacencyList(int n, int s, int t) {
-    super(n, s, t);
-  }
-
-  // Performs the Ford-Fulkerson method applying a depth first search as
-  // a means of finding an augmenting path.
-  @Override
-  public void solve() {
-    // Find max flow by adding all augmenting path flows.
-    for (long f = dfs(s, INF); f != 0; f = dfs(s, INF)) {
-      visitedToken++;
-      maxFlow += f;
+    public String toString(int s, int t) {
+      String u = (from == s) ? "s" : ((from == t) ? "t" : String.valueOf(from));
+      String v = (to == s) ? "s" : ((to == t) ? "t" : String.valueOf(to));
+      return String.format("Edge %s -> %s | flow = %3d | capacity = %3d | is residual: %s", 
+        u, v, flow, capacity, isResidual());
     }
   }
 
-  private long dfs(int node, long flow) {
-    // At sink node, return augmented path flow.
-    if (node == t) return flow;
+  private static abstract class NetworkFlowSolverBase {
 
-    List<Edge> edges = graph[node];
-    visited[node] = visitedToken;
+    // To avoid overflow, set infinity to a value less than Long.MAX_VALUE;
+    static final long INF = Long.MAX_VALUE / 2;
 
-    for (Edge edge : edges) {
-      if (edge.capacity > 0 && visited[edge.to] != visitedToken) {
-        long bottleNeck = dfs(edge.to, Math.min(flow, edge.capacity));
+    // Inputs: n = number of nodes, s = source, t = sink
+    final int n, s, t;
 
-        // Augment flow with bottle neck value
-        if (bottleNeck > 0) {
-          Edge res = edge.residual;
-          edge.flow += bottleNeck;
-          edge.capacity -= bottleNeck;
-          res.capacity += bottleNeck;
-          return bottleNeck;
-        }
+    // 'visited' and 'visitedToken' are variables used in graph sub-routines to 
+    // track whether a node has been visited or not. In particular, node 'i' was 
+    // recently visited if visited[i] == visitedToken is true. This is handy 
+    // because to mark all nodes as unvisited simply increment the visitedToken.
+    protected int visitedToken = 1;
+    protected int[] visited;
 
+    // Indicates whether the network flow algorithm has ran. The solver only 
+    // needs to run once because it always yields the same result.
+    protected boolean solved;
+
+    // The maximum flow. Calculated by calling the {@link #solve} method.
+    protected long maxFlow;
+
+    // The adjacency list representing the flow graph.
+    protected List<Edge>[] graph;
+
+    /**
+     * Creates an instance of a flow network solver. Use the {@link #addEdge}
+     * method to add edges to the graph.
+     *
+     * @param n - The number of nodes in the graph including s and t.
+     * @param s - The index of the source node, 0 <= s < n
+     * @param t - The index of the sink node, 0 <= t < n and t != s
+     */
+    public NetworkFlowSolverBase(int n, int s, int t) {
+      this.n = n; 
+      this.s = s; 
+      this.t = t; 
+      initializeEmptyFlowGraph();
+      visited = new int[n];
+    }
+
+    // Constructs an empty graph with n nodes including s and t.
+    private void initializeEmptyFlowGraph() {
+      graph = new List[n];
+      for (int i = 0; i < n; i++)
+        graph[i] = new ArrayList<Edge>();
+    }
+
+    /**
+     * Adds a directed edge (and its residual edge) to the flow graph.
+     *
+     * @param from     - The index of the node the directed edge starts at.
+     * @param to       - The index of the node the directed edge ends at.
+     * @param capacity - The capacity of the edge
+     */
+    public void addEdge(int from, int to, long capacity) {
+      if (capacity <= 0) 
+        throw new IllegalArgumentException("Forward edge capacity <= 0");
+      Edge e1 = new Edge(from, to, capacity);
+      Edge e2 = new Edge(to, from, 0);
+      e1.residual = e2;
+      e2.residual = e1;
+      graph[from].add(e1);
+      graph[to].add(e2);
+    }
+
+    /**
+     * Returns the residual graph after the solver has been executed. This 
+     * allows you to inspect the {@link Edge#flow} and {@link Edge#capacity}
+     * values of each edge. This is useful if you are debugging or want to 
+     * figure out which edges were used during the max flow.
+     */
+    public List<Edge>[] getGraph() {
+      execute();
+      return graph;
+    }
+
+    // Returns the maximum flow from the source to the sink.
+    public long getMaxFlow() {
+      execute();
+      return maxFlow;
+    }
+
+    // Wrapper method that ensures we only call solve() once
+    private void execute() {
+      if (solved) return; 
+      solved = true;
+      solve();
+    }
+
+    // Method to implement which solves the network flow problem.
+    public abstract void solve();
+
+  }
+
+  private static class FordFulkersonDfsSolver extends NetworkFlowSolverBase {
+
+    /**
+     * Creates an instance of a flow network solver. Use the {@link #addEdge}
+     * method to add edges to the graph.
+     *
+     * @param n - The number of nodes in the graph including s and t.
+     * @param s - The index of the source node, 0 <= s < n
+     * @param t - The index of the sink node, 0 <= t < n and t != s
+     */
+    public FordFulkersonDfsSolver(int n, int s, int t) {
+      super(n, s, t);
+    }
+
+    // Performs the Ford-Fulkerson method applying a depth first search as
+    // a means of finding an augmenting path.
+    @Override
+    public void solve() {
+      // Find max flow by adding all augmenting path flows.
+      for (long f = dfs(s, INF); f != 0; f = dfs(s, INF)) {
+        visitedToken++;
+        maxFlow += f;
       }
     }
-    return 0;
+
+    private long dfs(int node, long flow) {
+      // At sink node, return augmented path flow.
+      if (node == t) return flow;
+
+      // Mark the current node as visited.
+      visited[node] = visitedToken;
+
+      List<Edge> edges = graph[node];
+      for (Edge edge : edges) {
+        if (edge.remainingCapacity() > 0 && visited[edge.to] != visitedToken) {
+          long bottleNeck = dfs(edge.to, min(flow, edge.remainingCapacity()));
+
+          // If we made it from s -> t (a.k.a bottleNeck > 0) then
+          // augment flow with bottleneck value.
+          if (bottleNeck > 0) {
+            edge.augment(bottleNeck);
+            return bottleNeck;
+          }
+
+        }
+      }
+      return 0;
+    }
   }
+
 }
