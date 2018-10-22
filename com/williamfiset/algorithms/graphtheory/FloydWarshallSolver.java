5fd7496ec4f6bbe2d82f758799163ebdf5b24ae7
William Fiset <fisetwill@gmail.com>
2018-10-21 21:46:58 -0700

    Update FloydWarshallSolver.java

--- com/williamfiset/algorithms/graphtheory/FloydWarshallSolver.java
+++ com/williamfiset/algorithms/graphtheory/FloydWarshallSolver.java
@@ -20,98 +20,6 @@ import java.util.List;
 
 public class FloydWarshallSolver {
 
-    /* Example usage. */
-
-  // Creates a graph with n nodes. The adjacency matrix is constructed 
-  // such that the value of going from a node to itself is 0.
-  public static double[][] createGraph(int n) {
-    double[][] matrix = new double[n][n];
-    for (int i = 0; i < n; i++) {
-      java.util.Arrays.fill(matrix[i], POSITIVE_INFINITY);
-      matrix[i][i] = 0;
-    }
-    return matrix;
-  }
-
-  public static void main(String[] args) {
-    // Construct graph.
-    int n = 7;
-    double[][] m = createGraph(n);
-
-    // Add some edge values.
-    m[0][1] = 2;
-    m[0][2] = 5;
-    m[0][6] = 10;
-    m[1][2] = 2;
-    m[1][4] = 11;
-    m[2][6] = 2;
-    m[6][5] = 11;
-    m[4][5] = 1;
-    m[5][4] = -2;
-
-    FloydWarshallSolver solver = new FloydWarshallSolver(m);
-    double[][] dist = solver.getApspMatrix();
-
-    for (int i = 0; i < n; i++)
-      for (int j = 0; j < n; j++)
-        System.out.printf("This shortest path from node %d to node %d is %.3f\n", i, j, dist[i][j]);
-
-    // Prints:
-    // This shortest path from node 0 to node 0 is 0.000
-    // This shortest path from node 0 to node 1 is 2.000
-    // This shortest path from node 0 to node 2 is 4.000
-    // This shortest path from node 0 to node 3 is Infinity
-    // This shortest path from node 0 to node 4 is -Infinity
-    // This shortest path from node 0 to node 5 is -Infinity
-    // This shortest path from node 0 to node 6 is 6.000
-    // This shortest path from node 1 to node 0 is Infinity
-    // This shortest path from node 1 to node 1 is 0.000
-    // This shortest path from node 1 to node 2 is 2.000
-    // This shortest path from node 1 to node 3 is Infinity
-    // ...
-
-    System.out.println();
-
-    // Reconstructs the shortest paths from all nodes to every other nodes.
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        List<Integer> path = solver.reconstructShortestPath(i, j);
-        String str;
-        if (path == null) {
-          str = "HAS AN ∞ NUMBER OF SOLUTIONS! (negative cycle case)";
-        } else if (path.size() == 0) {
-          str = String.format("DOES NOT EXIST (node %d doesn't reach node %d)", i, j);
-        } else {
-          str = String.join(" -> ", path.stream()
-                                        .map(Object::toString)
-                                        .collect(java.util.stream.Collectors.toList()));
-          str = "is: [" + str + "]";
-        }
-
-        System.out.printf("The shortest path from node %d to node %d %s\n", i, j, str);
-      }
-    }
-
-    // Prints:
-    // The shortest path from node 0 to node 0 is: [0]
-    // The shortest path from node 0 to node 1 is: [0 -> 1]
-    // The shortest path from node 0 to node 2 is: [0 -> 1 -> 2]
-    // The shortest path from node 0 to node 3 DOES NOT EXIST (node 0 doesn't reach node 3)
-    // The shortest path from node 0 to node 4 HAS AN ∞ NUMBER OF SOLUTIONS! (negative cycle case)
-    // The shortest path from node 0 to node 5 HAS AN ∞ NUMBER OF SOLUTIONS! (negative cycle case)
-    // The shortest path from node 0 to node 6 is: [0 -> 1 -> 2 -> 6]
-    // The shortest path from node 1 to node 0 DOES NOT EXIST (node 1 doesn't reach node 0)
-    // The shortest path from node 1 to node 1 is: [1]
-    // The shortest path from node 1 to node 2 is: [1 -> 2]
-    // The shortest path from node 1 to node 3 DOES NOT EXIST (node 1 doesn't reach node 3)
-    // The shortest path from node 1 to node 4 HAS AN ∞ NUMBER OF SOLUTIONS! (negative cycle case)
-    // The shortest path from node 1 to node 5 HAS AN ∞ NUMBER OF SOLUTIONS! (negative cycle case)
-    // The shortest path from node 1 to node 6 is: [1 -> 2 -> 6]
-    // The shortest path from node 2 to node 0 DOES NOT EXIST (node 2 doesn't reach node 0)
-    // ...
-    
-  }
-
   private int n;
   private boolean solved;
   private double[][] dp;
@@ -204,6 +112,96 @@ public class FloydWarshallSolver {
     return path;
   }
 
+    /* Example usage. */
+
+  // Creates a graph with n nodes. The adjacency matrix is constructed 
+  // such that the value of going from a node to itself is 0.
+  public static double[][] createGraph(int n) {
+    double[][] matrix = new double[n][n];
+    for (int i = 0; i < n; i++) {
+      java.util.Arrays.fill(matrix[i], POSITIVE_INFINITY);
+      matrix[i][i] = 0;
+    }
+    return matrix;
+  }
+
+  public static void main(String[] args) {
+    // Construct graph.
+    int n = 7;
+    double[][] m = createGraph(n);
+
+    // Add some edge values.
+    m[0][1] = 2;
+    m[0][2] = 5;
+    m[0][6] = 10;
+    m[1][2] = 2;
+    m[1][4] = 11;
+    m[2][6] = 2;
+    m[6][5] = 11;
+    m[4][5] = 1;
+    m[5][4] = -2;
+
+    FloydWarshallSolver solver = new FloydWarshallSolver(m);
+    double[][] dist = solver.getApspMatrix();
+
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < n; j++)
+        System.out.printf("This shortest path from node %d to node %d is %.3f\n", i, j, dist[i][j]);
+
+    // Prints:
+    // This shortest path from node 0 to node 0 is 0.000
+    // This shortest path from node 0 to node 1 is 2.000
+    // This shortest path from node 0 to node 2 is 4.000
+    // This shortest path from node 0 to node 3 is Infinity
+    // This shortest path from node 0 to node 4 is -Infinity
+    // This shortest path from node 0 to node 5 is -Infinity
+    // This shortest path from node 0 to node 6 is 6.000
+    // This shortest path from node 1 to node 0 is Infinity
+    // This shortest path from node 1 to node 1 is 0.000
+    // This shortest path from node 1 to node 2 is 2.000
+    // This shortest path from node 1 to node 3 is Infinity
+    // ...
+
+    System.out.println();
+
+    // Reconstructs the shortest paths from all nodes to every other nodes.
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        List<Integer> path = solver.reconstructShortestPath(i, j);
+        String str;
+        if (path == null) {
+          str = "HAS AN ∞ NUMBER OF SOLUTIONS! (negative cycle case)";
+        } else if (path.size() == 0) {
+          str = String.format("DOES NOT EXIST (node %d doesn't reach node %d)", i, j);
+        } else {
+          str = String.join(" -> ", path.stream()
+                                        .map(Object::toString)
+                                        .collect(java.util.stream.Collectors.toList()));
+          str = "is: [" + str + "]";
+        }
+
+        System.out.printf("The shortest path from node %d to node %d %s\n", i, j, str);
+      }
+    }
+
+    // Prints:
+    // The shortest path from node 0 to node 0 is: [0]
+    // The shortest path from node 0 to node 1 is: [0 -> 1]
+    // The shortest path from node 0 to node 2 is: [0 -> 1 -> 2]
+    // The shortest path from node 0 to node 3 DOES NOT EXIST (node 0 doesn't reach node 3)
+    // The shortest path from node 0 to node 4 HAS AN ∞ NUMBER OF SOLUTIONS! (negative cycle case)
+    // The shortest path from node 0 to node 5 HAS AN ∞ NUMBER OF SOLUTIONS! (negative cycle case)
+    // The shortest path from node 0 to node 6 is: [0 -> 1 -> 2 -> 6]
+    // The shortest path from node 1 to node 0 DOES NOT EXIST (node 1 doesn't reach node 0)
+    // The shortest path from node 1 to node 1 is: [1]
+    // The shortest path from node 1 to node 2 is: [1 -> 2]
+    // The shortest path from node 1 to node 3 DOES NOT EXIST (node 1 doesn't reach node 3)
+    // The shortest path from node 1 to node 4 HAS AN ∞ NUMBER OF SOLUTIONS! (negative cycle case)
+    // The shortest path from node 1 to node 5 HAS AN ∞ NUMBER OF SOLUTIONS! (negative cycle case)
+    // The shortest path from node 1 to node 6 is: [1 -> 2 -> 6]
+    // The shortest path from node 2 to node 0 DOES NOT EXIST (node 2 doesn't reach node 0)
+    // ... 
+  }
 }
 
 
