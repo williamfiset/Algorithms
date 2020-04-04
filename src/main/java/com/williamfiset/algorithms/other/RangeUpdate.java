8b30f9a090bfff75c8536c7b7fcd9b1aa3575c01
AtharvaThorve <aaathorve@gmail.com>
2020-04-04 10:47:09 +0530

    Suggested changes added

--- src/main/java/com/williamfiset/algorithms/other/RangeUpdate.java
+++ src/main/java/com/williamfiset/algorithms/other/RangeUpdate.java
@@ -1,5 +1,6 @@
 /**
- * Update range of an array from index l to index r by value x by difference array method
+ * Update range of an array from index l to index r by value x by difference array method.
+ * RangeUpdate is primarily used for offline algorithms
  *
  * <p>Time complexity to update O(1) but time complexity to print is still O(n);
  *
@@ -9,10 +10,19 @@ package com.williamfiset.algorithms.other;
 
 public class RangeUpdate {
 
+  private int[] array;
+  private int[] differenceArray;
+
+  // Assigns the value to the array instance.
   // Creates a diff array differenceArray[] for array[] and returns
-  // it after filling initial values.
-  public static void initializeDiffArray(int[] array, int[] differenceArray) {
+  // it after filling initial values. The size of the difference array needs
+  // to be 1 greater than array to be worked on.
+  // We use one extra space because
+  // update(l, r, x) updates D[r+1]
+  public RangeUpdate(int[] array) {
+    this.array = array;
     int n = array.length;
+    differenceArray = new int[n + 1];
     differenceArray[0] = array[0];
     differenceArray[n] = 0;
     for (int i = 1; i < n; i++) {
@@ -21,13 +31,13 @@ public class RangeUpdate {
   }
 
   // Does range update
-  public static void update(int[] differenceArray, int l, int r, int x) {
+  public void update(int l, int r, int x) {
     differenceArray[l] += x;
     differenceArray[r + 1] -= x;
   }
 
   // Call this method after all the queries are done
-  public static void updateArray(int[] array, int[] differenceArray) {
+  public void finalize() {
     int n = array.length;
     for (int i = 0; i < n; i++) {
       if (i == 0) {
@@ -41,36 +51,23 @@ public class RangeUpdate {
     }
   }
 
-  public static void printArray(int[] array) {
-    int n = array.length;
-    for (int i = 0; i < n; i++) {
-      System.out.print(array[i] + " ");
-    }
-    System.out.println();
-  }
-
   public static void main(String[] args) {
 
     // Array to be updated
     int[] array = {10, 4, 6, 13, 8, 15, 17, 22};
-    int n = array.length;
-    // Create and fill difference Array
-    // We use one extra space because
-    // update(l, r, x) updates D[r+1]
-    int differenceArray[] = new int[n + 1];
-    initializeDiffArray(array, differenceArray);
+    RangeUpdate rangeUpdate = new RangeUpdate(array);
 
     // After below update(l, r, x), the
-    // elements should become 10, 14, 16, 23, 18, 15, 17, 22
-    update(differenceArray, 1, 4, 10);
-    updateArray(array, differenceArray);
-    printArray(array);
+    // elements should become [10, 14, 16, 23, 18, 15, 17, 22]
+    rangeUpdate.update(1, 4, 10);
+    rangeUpdate.finalize();
+    System.out.println(java.util.Arrays.toString(array));
 
     // After below update(l, r, x), the
-    // elements should become 22, 26, 28, 30, 25, 22, 24, 34
-    update(differenceArray, 3, 6, -5);
-    update(differenceArray, 0, 7, 12);
-    updateArray(array, differenceArray);
-    printArray(array);
+    // elements should become [22, 26, 28, 30, 25, 22, 24, 34]
+    rangeUpdate.update(3, 6, -5);
+    rangeUpdate.update(0, 7, 12);
+    rangeUpdate.finalize();
+    System.out.println(java.util.Arrays.toString(array));
   }
 }
--- src/test/java/com/williamfiset/algorithms/other/RangeUpdateTest.java
+++ src/test/java/com/williamfiset/algorithms/other/RangeUpdateTest.java
@@ -5,21 +5,15 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class RangeUpdateTest {
-  private static RangeUpdate rangeUpdate;
-
-  public static void prepareTest() {
-    rangeUpdate = new RangeUpdate();
-  }
 
   @Test
   public void RangeUpdateTest1() {
     int[] a = {10, 5, 20, 40};
-    int[] d = new int[a.length + 1];
-    rangeUpdate.initializeDiffArray(a, d);
-    rangeUpdate.update(d, 0, 1, 10);
-    rangeUpdate.update(d, 1, 3, 20);
-    rangeUpdate.update(d, 2, 2, 30);
-    rangeUpdate.updateArray(a, d);
+    RangeUpdate rangeUpdate = new RangeUpdate(a);
+    rangeUpdate.update(0, 1, 10);
+    rangeUpdate.update(1, 3, 20);
+    rangeUpdate.update(2, 2, 30);
+    rangeUpdate.finalize();
     int[] expected = {20, 35, 70, 60};
     assertArrayEquals(expected, a);
   }
@@ -27,12 +21,11 @@ public class RangeUpdateTest {
   @Test
   public void RangeUpdateTest2() {
     int[] a = {270, 311, 427, 535, 334, 193, 174};
-    int[] d = new int[a.length + 1];
-    rangeUpdate.initializeDiffArray(a, d);
-    rangeUpdate.update(d, 2, 5, 32);
-    rangeUpdate.update(d, 0, 4, 101);
-    rangeUpdate.update(d, 5, 6, -73);
-    rangeUpdate.updateArray(a, d);
+    RangeUpdate rangeUpdate = new RangeUpdate(a);
+    rangeUpdate.update(2, 5, 32);
+    rangeUpdate.update(0, 4, 101);
+    rangeUpdate.update(5, 6, -73);
+    rangeUpdate.finalize();
     int[] expected = {371, 412, 560, 668, 467, 152, 101};
     assertArrayEquals(expected, a);
   }
