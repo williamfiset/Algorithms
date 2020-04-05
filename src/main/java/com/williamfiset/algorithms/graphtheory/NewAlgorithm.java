4a1196f3da0b40ee5262ec3ebf933fecb3c32a52
William Fiset <williamfiset@Williams-Mac-mini.local>
2020-04-05 12:02:09 -0700

    Test precommit hook and other workflows

---
+++ src/main/java/com/williamfiset/algorithms/graphtheory/NewAlgorithm.java
@@ -0,0 +1,27 @@
+package com.williamfiset.algorithms.graphtheory;
+
+public class NewAlgorithm      {
+
+
+          public static void main(String[] args) {
+            
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
