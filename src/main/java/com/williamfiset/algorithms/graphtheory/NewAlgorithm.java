c5025364474411d8965bb22ffce434fbe228b86a
William Fiset <williamfiset@Williams-Mac-mini.local>
2020-04-05 13:12:32 -0700

    test pre-push hook

--- src/main/java/com/williamfiset/algorithms/graphtheory/NewAlgorithm.java
+++ src/main/java/com/williamfiset/algorithms/graphtheory/NewAlgorithm.java
@@ -2,5 +2,17 @@ package com.williamfiset.algorithms.graphtheory;
 
 public class NewAlgorithm {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+
+                       }
 }
