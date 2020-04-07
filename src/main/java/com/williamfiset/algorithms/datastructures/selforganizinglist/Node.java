8af10c475d3daf56942db9b2789a387e336fed72
Alexandra Englert <rubi_81@outlook.de>
2020-04-07 20:53:11 +0200

    indentation

--- src/main/java/com/williamfiset/algorithms/datastructures/selforganizinglist/Node.java
+++ src/main/java/com/williamfiset/algorithms/datastructures/selforganizinglist/Node.java
@@ -4,16 +4,15 @@ package com.williamfiset.algorithms.datastructures.selforganizinglist;
  * Class representing the elements of the list
  */
 public class Node {
-	int data;
-	int count;
-	Node prev;
-	Node next;
-	
-	public Node(int data) {
-		this.data = data;
-		this.count = 0;
-		this.prev = null;
-		this.next = null;
-	}
+  int data;
+  int count;
+  Node prev;
+  Node next;
 
+  public Node(int data) {
+    this.data = data;
+    this.count = 0;
+    this.prev = null;
+    this.next = null;
+  }
 }
