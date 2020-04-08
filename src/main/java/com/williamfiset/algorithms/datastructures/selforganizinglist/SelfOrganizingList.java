62a747d29bf71670678e8326c9b6900bb74d2517
Alexandra Englert <rubi_81@outlook.de>
2020-04-08 12:49:54 +0200

    Update SelfOrganizingList.java
    implemeting search method

--- src/main/java/com/williamfiset/algorithms/datastructures/selforganizinglist/SelfOrganizingList.java
+++ src/main/java/com/williamfiset/algorithms/datastructures/selforganizinglist/SelfOrganizingList.java
@@ -23,4 +23,42 @@ public class SelfOrganizingList {
     }
     totalNodes++;
   }
+  
+  public void search(int key) {
+	  Node current = start;
+	  if (current==null) {
+		  System.out.println("List is empty");
+	  } else {
+		  while (current != null) {
+			  if (key == current.data) {
+				  current.count++;
+				  reorder();
+				  break;
+			  }
+			  current = current.next;
+		  }
+		  if (current==null) {
+			  System.out.println("Element " + key + " was not found");
+		  } else {
+			  System.out.println(key + " was found");
+		  }
+	  }
+  }
+  
+  public void reorder() {
+	  Node current = start;
+	  int tempdata;
+	  int tempcount;
+	  for (int i=1;i<totalNodes;i++) {
+		  if (current.count < current.next.count) {
+			  tempdata = current.data;
+			  tempcount = current.count;
+			  current.data = current.next.data;
+			  current.count = current.next.count;
+			  current.next.data = tempdata;
+			  current.next.count = tempcount;
+		  }
+		  current = current.next;
+	  }
+  }
 }
