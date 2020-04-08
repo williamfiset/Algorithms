ccc0c7752476c972a2dd3facbe9c47829dcf8366
Alexandra Englert <rubi_81@outlook.de>
2020-04-08 17:28:07 +0200

    Update SelfOrganizingListTest.java
    indentation

--- src/test/java/com/williamfiset/algorithms/datastructures/selforganizinglist/SelfOrganizingListTest.java
+++ src/test/java/com/williamfiset/algorithms/datastructures/selforganizinglist/SelfOrganizingListTest.java
@@ -28,18 +28,18 @@ class SelfOrganizingListTest {
     assertTrue(list.totalNodes == 3);
     assertTrue(list.start.next.next.data == 42);
   }
-  
+
   @Test
   void testReorder() {
-	  list.insertNode(12);
-	  list.insertNode(7);
-	  list.insertNode(24);
-	  list.search(7);
-	  assertTrue(list.start.data == 7);
-	  assertTrue(list.start.count == 1);
-	  list.search(24);
-	  list.search(24);
-	  assertTrue(list.start.data == 24);
-	  assertTrue(list.start.count == 2);
+    list.insertNode(12);
+    list.insertNode(7);
+    list.insertNode(24);
+    list.search(7);
+    assertTrue(list.start.data == 7);
+    assertTrue(list.start.count == 1);
+    list.search(24);
+    list.search(24);
+    assertTrue(list.start.data == 24);
+    assertTrue(list.start.count == 2);
   }
 }
