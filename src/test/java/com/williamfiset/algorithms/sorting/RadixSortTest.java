ad7ebf396ee998c6c0e0b09eba3c0f41affdbb83
Alexandra Englert <rubi_81@outlook.de>
2020-04-09 14:58:10 +0200

    Create RadixSortTest.java
    implement test case

---
+++ src/test/java/com/williamfiset/algorithms/sorting/RadixSortTest.java
@@ -0,0 +1,28 @@
+package com.williamfiset.algorithms.sorting;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class RadixSortTest {
+  RadixSort sort;
+
+  @Test
+  public void testGetMax() {
+    int[] array = {3, 90, 112, 56};
+    int[] array2 = {5, 9, 1, 42, 117};
+    int max = RadixSort.getMax(array);
+    int max2 = RadixSort.getMax(array2);
+    assertEquals(112, max);
+    assertEquals(117, max2);
+  }
+
+  @Test
+  public void testRadixSort() {
+    int[] array = {90, 23, 101, 45, 65, 23, 67, 89, 34, 23};
+    RadixSort.radixSort(array);
+    assertEquals(23, array[0]);
+    assertEquals(101, array[array.length - 1]);
+    assertEquals(34, array[3]);
+  }
+}
