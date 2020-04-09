6aabcde7207ccd8290fb67f99b64c70687b3bfd0
Alexandra Englert <rubi_81@outlook.de>
2020-04-09 18:01:58 +0200

    Update QuickSelectTest.java
    indentation

--- src/test/java/com/williamfiset/algorithms/search/QuickSelectTest.java
+++ src/test/java/com/williamfiset/algorithms/search/QuickSelectTest.java
@@ -6,13 +6,12 @@ import org.junit.Test;
 
 public class QuickSelectTest {
 
-	@Test
-	public void testQuickSelect() {
-	    int array[] = {1, 89, 2, 17, 42};
-	    
-	    assertEquals(89, QuickSelect.findKthLargest(array, 1));
-	    assertEquals(1, QuickSelect.findKthLargest(array, 5));
-	    assertEquals(17, QuickSelect.findKthLargest(array, 3));
-	  }
+  @Test
+  public void testQuickSelect() {
+    int array[] = {1, 89, 2, 17, 42};
 
+    assertEquals(89, QuickSelect.findKthLargest(array, 1));
+    assertEquals(1, QuickSelect.findKthLargest(array, 5));
+    assertEquals(17, QuickSelect.findKthLargest(array, 3));
+  }
 }
