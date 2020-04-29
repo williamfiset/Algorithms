017d8fe7fa08e2b20305753c6b958bc4259d2ba5
William Fiset <williamfiset@Williams-Mac-mini.local>
2020-04-28 18:52:59 -0700

    DP dominos

--- src/main/java/com/williamfiset/algorithms/dp/examples/tilingdominoes/TilingDominoes.java
+++ src/main/java/com/williamfiset/algorithms/dp/examples/tilingdominoes/TilingDominoes.java
@@ -8,7 +8,7 @@ public class TilingDominoes {
       int n = sc.nextInt();
       if (n == -1) break;
       System.out.println(solution1(n));
-      System.out.println(solution2(n));
+      // System.out.println(solution2(n));
     }
   }
 
@@ -22,9 +22,9 @@ public class TilingDominoes {
   //     0       0       1       1
   //     1       1       1       1
   private static int solution1(int n) {
-    int[][] dp = new int[n + 2][1 << 3];
-    dp[1][7] = 1;
-    for (int i = 2; i < n + 2; i++) {
+    int[][] dp = new int[n + 1][1 << 3];
+    dp[0][7] = 1;
+    for (int i = 1; i < n + 1; i++) {
       // The number of empty states for this column is the number
       // of full states in the previous column.
       dp[i][0] += dp[i - 1][7];
@@ -47,9 +47,9 @@ public class TilingDominoes {
       dp[i][6] += dp[i - 1][7];
       dp[i][6] += dp[i - 1][1]; // Don't double because placing a block sideways double includes?
 
-      dp[i][7] += dp[i][1];
-      dp[i][7] += dp[i][4];
-      dp[i][7] += dp[i - 2][7]; // Three sideways blocks
+      dp[i][7] += dp[i - 1][3];
+      dp[i][7] += dp[i - 1][6];
+      dp[i][7] += dp[i - 1][0]; // Three sideways blocks
     }
     // printMatrix(dp);
     return dp[dp.length - 1][7];
