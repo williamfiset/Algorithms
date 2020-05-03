22a344e55739ea8749382205e8081f55a4447151
Alexandra Englert <rubi_81@outlook.de>
2020-05-03 14:54:44 +0200

    Correct indentation

--- src/main/java/com/williamfiset/algorithms/sorting/RadixSort.java
+++ src/main/java/com/williamfiset/algorithms/sorting/RadixSort.java
@@ -1,55 +1,51 @@
 package com.williamfiset.algorithms.sorting;
 
 public class RadixSort {
-	static int getMax(int[] array) {
-		int max = array[0];
-	    for (int i = 0; i < array.length; i++) {
-	      if (array[i] > max) {
-	        max = array[i];
-	      }
-	    }
-	    return max;
+  static int getMax(int[] array) {
+    int max = array[0];
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] > max) {
+        max = array[i];
+      }
     }
-	
-	static int calculateNumberOfDigits(int number) {
-        return (int) Math.log10(number) + 1;
+    return max;
+  }
+
+  static int calculateNumberOfDigits(int number) {
+    return (int) Math.log10(number) + 1;
+  }
+
+  public static void radixSort(int numbers[]) {
+    int maximum = getMax(numbers);
+    int numberOfDigits = calculateNumberOfDigits(maximum);
+    int placeValue = 1;
+    while (numberOfDigits-- > 0) {
+      countSort(numbers, placeValue);
+      placeValue *= 10;
+    }
+  }
+
+  private static void countSort(int[] numbers, int placeValue) {
+    int range = 10;
+
+    int[] frequency = new int[range];
+    int[] sortedValues = new int[numbers.length];
+
+    for (int i = 0; i < numbers.length; i++) {
+      int digit = (numbers[i] / placeValue) % range;
+      frequency[digit]++;
     }
 
-    public static void radixSort(int numbers[]) {
-        int maximum = getMax(numbers);
-        int numberOfDigits = calculateNumberOfDigits(maximum);
-        int placeValue = 1;
-        while (numberOfDigits-- > 0) {
-            countSort(numbers, placeValue);
-            placeValue *= 10;
-        }
+    for (int i = 1; i < range; i++) {
+      frequency[i] += frequency[i - 1];
     }
 
-    private static void countSort(int[] numbers, int placeValue) {
-        int range = 10; 
-        
-        int[] frequency = new int[range];
-        int[] sortedValues = new int[numbers.length];
-
-        for (int i = 0; i < numbers.length; i++) {
-            int digit = (numbers[i] / placeValue) % range;
-            frequency[digit]++;
-        }
-
-        for (int i = 1; i < range; i++) {
-            frequency[i] += frequency[i - 1];
-        }
-
-        for (int i = numbers.length - 1; i >= 0; i--) {
-            int digit = (numbers[i] / placeValue) % range;
-            sortedValues[frequency[digit] - 1] = numbers[i];
-            frequency[digit]--;
-        }
-
-        System.arraycopy(sortedValues, 0, numbers, 0, numbers.length);
-        
+    for (int i = numbers.length - 1; i >= 0; i--) {
+      int digit = (numbers[i] / placeValue) % range;
+      sortedValues[frequency[digit] - 1] = numbers[i];
+      frequency[digit]--;
     }
-    
-    
 
+    System.arraycopy(sortedValues, 0, numbers, 0, numbers.length);
+  }
 }
--- src/test/java/com/williamfiset/algorithms/sorting/RadixSortTest.java
+++ src/test/java/com/williamfiset/algorithms/sorting/RadixSortTest.java
@@ -6,15 +6,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.Test;
 
 public class RadixSortTest {
-	@Test
-	void testGetMax() {
-		int[] array = {5, 7, 1, 13, 42, 101, 512, 67, 1013, 287, 912};
-		assertEquals(1013, RadixSort.getMax(array));
-	}
-	
-	@Test
-	void testCalculateNumberOfDigits() {
-		assertTrue(4==RadixSort.calculateNumberOfDigits(1089));
-		assertTrue(2==RadixSort.calculateNumberOfDigits(19));
-	}
+  @Test
+  void testGetMax() {
+    int[] array = {5, 7, 1, 13, 42, 101, 512, 67, 1013, 287, 912};
+    assertEquals(1013, RadixSort.getMax(array));
+  }
+
+  @Test
+  void testCalculateNumberOfDigits() {
+    assertTrue(4 == RadixSort.calculateNumberOfDigits(1089));
+    assertTrue(2 == RadixSort.calculateNumberOfDigits(19));
+  }
 }
