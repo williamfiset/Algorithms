c32847b7c1bcb1b222bfa3189b40403974a87941
Alexandra Englert <rubi_81@outlook.de>
2020-05-03 15:00:12 +0200

    Update RadixSortTest.java
    make test methods public

--- src/test/java/com/williamfiset/algorithms/sorting/RadixSortTest.java
+++ src/test/java/com/williamfiset/algorithms/sorting/RadixSortTest.java
@@ -7,13 +7,13 @@ import org.junit.Test;
 
 public class RadixSortTest {
   @Test
-  void testGetMax() {
+  public void testGetMax() {
     int[] array = {5, 7, 1, 13, 42, 101, 512, 67, 1013, 287, 912};
     assertEquals(1013, RadixSort.getMax(array));
   }
 
   @Test
-  void testCalculateNumberOfDigits() {
+  public void testCalculateNumberOfDigits() {
     assertTrue(4 == RadixSort.calculateNumberOfDigits(1089));
     assertTrue(2 == RadixSort.calculateNumberOfDigits(19));
   }
