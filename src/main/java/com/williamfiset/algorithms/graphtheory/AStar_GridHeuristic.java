2166478a038715e2317fd7b4056f7d33097d5e3e
Nishant Chatterjee <nishantc1527@gmail.com>
2020-05-15 16:41:49 -0700

    added a*

--- src/main/java/com/williamfiset/algorithms/graphtheory/AStar_GridHeuristic.java
+++ src/main/java/com/williamfiset/algorithms/graphtheory/AStar_GridHeuristic.java
@@ -1,279 +1,229 @@
-/** NOTE: This algorithm is still a work in progress! See issue #18 to track progress. */
 package com.williamfiset.algorithms.graphtheory;
 
 import java.util.*;
 
+/**
+ * An A* algorithm implementation for a adjacency list.
+ *
+ * @see DijkstrasShortestPathAdjacencyList
+ * @author Nishant Chatterjee, nishatc1527@gmail.com
+ */
 public class AStar_GridHeuristic {
 
-  // An edge class to represent a directed edge
-  // between two nodes with a certain non-negative cost.
-  static class Edge {
-    double cost;
-    int from, to;
+  public static class Pair<A, B> {
 
-    public Edge(int from, int to, double cost) {
-      if (cost < 0) throw new IllegalArgumentException("No negative edge weights");
-      this.from = from;
-      this.to = to;
-      this.cost = cost;
+    public final A fst;
+    public final B snd;
+
+    public Pair(A fst, B snd) {
+      this.fst = fst;
+      this.snd = snd;
     }
 
-    @Override
     public String toString() {
-      return from + ":" + to;
+      return "Pair[" + fst + "," + snd + "]";
+    }
+
+    public boolean equals(Object other) {
+      return other instanceof Pair<?, ?>
+          && Objects.equals(fst, ((Pair<?, ?>) other).fst)
+          && Objects.equals(snd, ((Pair<?, ?>) other).snd);
+    }
+
+    public int hashCode() {
+      if (fst == null) return (snd == null) ? 0 : snd.hashCode() + 1;
+      else if (snd == null) return fst.hashCode() + 2;
+      else return fst.hashCode() * 17 + snd.hashCode();
+    }
+
+    public static <A, B> Pair<A, B> of(A a, B b) {
+      return new Pair<>(a, b);
     }
   }
 
-  // Node class to track the nodes to visit while running A*
-  private static class Node implements Comparable<Node> {
+  // Create a graph with V vertices
+  @SuppressWarnings("unchecked")
+  public static List<Edge>[] createGraph(final int V) {
+    List<Edge>[] graph = new List[V];
+    for (int i = 0; i < V; i++) graph[i] = new ArrayList<>();
+    return graph;
+  }
 
-    int id;
-    double f, g, h;
+  // Helper function to add an edge to the graph
+  public static void addEdge(List<Edge>[] graph, int from, int to, int cost) {
+    graph[from].add(new Edge(to, cost));
+  }
 
-    private static final double EPS = 1e-7;
+  /**
+   * The main A* algorithm.
+   *
+   * @param graph The graph containing the vertices and adjacency list.
+   * @param start The starting vertex.
+   * @param end The target vertex.
+   * @return The smallest distance with the weights from the start to end.
+   */
+  public static int aStar(List<Edge>[] graph, int start, int end) {
+    final int V = graph.length;
+    int[] distances = new int[V];
+    int[] heuristics = heuristics(graph, end);
+    boolean[] visited = new boolean[V];
+    int visitedCount = 0;
+    Arrays.fill(distances, Integer.MAX_VALUE);
+    distances[start] = 0;
 
-    public Node(int nodeID, double gg, double hh) {
-      id = nodeID;
-      g = gg;
-      h = hh;
-      f = g + h;
-    }
+    while (visitedCount < V) {
+      int nextMin = chooseNext(heuristics, distances, visited);
+      visitedCount++;
 
-    // Sort by f cost and break ties with h
-    @Override
-    public int compareTo(Node other) {
-      if (Math.abs(f - other.f) < EPS) {
-        if (Math.abs(h - other.h) < EPS) return 0;
-        return (h - other.h) > 0 ? +1 : -1;
+      distances[nextMin] = distances[nextMin];
+      if (nextMin == end) {
+        return distances[nextMin];
       }
-      return (f - other.f) > 0 ? +1 : -1;
-    }
-  }
 
-  // Run A* algorithm on a directed graph to find the shortest path
-  // from a starting node to an ending node. If there is no path between the
-  // starting node and the destination node the returned value is set to be
-  // Double.POSITIVE_INFINITY.
-  public static double astar(
-      double[] X, double[] Y, Map<Integer, List<Edge>> graph, int start, int end, int n) {
+      List<Edge> neighbors = graph[nextMin];
 
-    // In the event that you wish to rebuild the shortest path
-    // you can do so using the prev array and starting at some node 'end'
-    // and finding the previous node using prev[end] and the previous node
-    // after that prev[prev[end]] etc... working all the way back until
-    // the index of start is found. Simply uncomment where the prev array is used.
-    // int[] prev = new int[n];
-
-    double[] G = new double[n];
-    double[] F = new double[n];
-    Arrays.fill(G, Double.POSITIVE_INFINITY);
-    Arrays.fill(F, Double.POSITIVE_INFINITY);
-    G[start] = 0;
-    F[start] = heuristic(X, Y, start, end);
-
-    Set<Integer> openSet = new HashSet<>();
-    Set<Integer> closedSet = new HashSet<>();
-
-    // Keep a priority queue of the next most promising node to visit
-    PriorityQueue<Node> pq = new PriorityQueue<>();
-    pq.offer(new Node(start, G[start], F[start]));
-    openSet.add(start);
-
-    while (!pq.isEmpty()) {
-
-      Node node = pq.poll();
-      openSet.remove(node.id);
-      closedSet.add(node.id);
-
-      if (node.id == end) return G[end];
-
-      List<Edge> edges = graph.get(node.id);
-      if (edges != null) {
-        for (int i = 0; i < edges.size(); i++) {
-
-          Edge edge = edges.get(i);
-          if (closedSet.contains(edge.to)) continue;
-
-          double g = node.g + edge.cost;
-          double h = heuristic(X, Y, edge.to, end);
-
-          if (g < G[edge.to] || !openSet.contains(edge.to)) {
-
-            G[edge.to] = g;
-            // prev[edge.to] = edge.from;
-
-            if (!openSet.contains(edge.to)) {
-              pq.offer(new Node(edge.to, g, h));
-              openSet.add(edge.to);
-            }
-          }
+      for (Edge edge : neighbors) {
+        if (distances[nextMin] + edge.weight < distances[edge.to]) {
+          distances[edge.to] = distances[nextMin] + edge.weight;
         }
       }
     }
 
-    // End node is unreachable
-    return Double.POSITIVE_INFINITY;
+    return Integer.MAX_VALUE;
   }
 
-  // Euclidean distance is used as a heuristic
-  static double heuristic(double[] X, double[] Y, int at, int end) {
-    // double dx = Math.abs(X[at] - X[end]);
-    // double dy = Math.abs(Y[at] - Y[end]);
-    // return dx*dx + dy*dy; // Avoid square root
-    // return Math.sqrt(dx*dx + dy*dy);
-    return 0;
+  /**
+   * Chooses the next smallest vertex according to heuristic values and the smallest distance
+   * calculated so far.
+   *
+   * @param heuristics The heuristic values.
+   * @param distances The smallest distances found so far.
+   * @param visited The vertices already visited.
+   * @return The next smallest vertex.
+   */
+  private static int chooseNext(int[] heuristics, int[] distances, boolean[] visited) {
+    final int V = distances.length;
+    long minVal = Integer.MAX_VALUE;
+    int min = -1;
+    for (int i = 0; i < V; i++) {
+      if (visited[i]) {
+        continue;
+      }
+
+      long nextVal = Math.abs((long) heuristics[i] + (long) distances[i]);
+
+      if (nextVal < minVal) {
+        minVal = nextVal;
+        min = i;
+      }
+    }
+
+    visited[min] = true;
+    return min;
   }
 
-  // Run a random test between A* and Dijkstra
+  /**
+   * Fills the heuristic values and puts them in an array. The heuristic value of a vertex is
+   * calculated by the distance from that vertex to the ending vertex, which can easily be done
+   * using breadth first search. Note that you can use whatever technique to fill the heuristic
+   * values.
+   *
+   * @param graph The graph that contains the vertices.
+   * @param end The end vertex.
+   * @return An array resembling the heuristic value of each vertex.
+   */
+  private static int[] heuristics(List<Edge>[] graph, int end) {
+    final int V = graph.length;
+    int[] heuristics = new int[V];
+
+    for (int i = 0; i < graph.length; i++) {
+      heuristics[i] =
+          minDist(
+              graph, i,
+              end); // Simply fill each heuristic with the minimum distance from source to target.
+    }
+
+    return heuristics;
+  }
+
+  /**
+   * Calculates the shortest path from a start vertex to an end vertex.
+   *
+   * @param graph The graph containing the vertices.
+   * @param start The start vertex.
+   * @param end The ending vertex.
+   * @return The smallest path from the start to end vertex.
+   */
+  private static int minDist(List<Edge>[] graph, int start, int end) {
+    Queue<Pair<Integer, Integer>> queue =
+        new LinkedList<>(); // A queue where each entry is the vertex and the depth of the vertex.
+    queue.offer(Pair.of(start, 0));
+    boolean[] visited = new boolean[graph.length]; // To check if the vertex is visited.
+    visited[start] = true;
+
+    while (!queue.isEmpty()) {
+      Pair<Integer, Integer> next = queue.poll();
+      int nextVertex = next.fst;
+      int nextDepth = next.snd;
+
+      if (nextVertex == end) { // Reached target
+        return nextDepth;
+      }
+
+      List<Edge> neighbors = graph[nextVertex];
+
+      for (Edge edge : neighbors) {
+        if (!visited[edge.to]) {
+          queue.offer(Pair.of(edge.to, nextDepth + 1)); // Add every neighbor that wasn't visited.
+        }
+      }
+    }
+
+    return Integer.MAX_VALUE;
+  }
+
   public static void main(String[] args) {
+    List<Edge>[] graph = createGraph(5);
 
-    Random RANDOM = new Random();
+    // 0 -> 1: 6
+    // 1 -> 2: 5
+    // 3 -> 2: 3
+    // 0 -> 4: 1
+    // 4 -> 1: 1
+    // 4 -> 3: 2
 
-    int n = 20 * 20;
-    Map<Integer, List<Edge>> graph = new HashMap<>();
-    for (int i = 0; i < n; i++) graph.put(i, new ArrayList<>());
+    // 0 -> 1 ↘
+    // ⬇ ↗     2
+    // 4 -> 3 ↗
 
-    double[] X = new double[n];
-    double[] Y = new double[n];
-    int N = (int) Math.sqrt(n);
+    addEdge(graph, 0, 1, 6);
+    addEdge(graph, 1, 2, 5);
+    addEdge(graph, 3, 2, 3);
+    addEdge(graph, 0, 4, 1);
+    addEdge(graph, 4, 1, 1);
+    addEdge(graph, 4, 3, 2);
 
-    int connections = (n * (n - 1));
-    int[] locations = new int[connections * 2];
-
-    boolean[][] m = new boolean[n][n];
-
-    for (int k = 0; k < connections; k++) {
-
-      int i = RANDOM.nextInt(N);
-      int j = RANDOM.nextInt(N);
-      int ii = RANDOM.nextInt(N);
-      int jj = RANDOM.nextInt(N);
-
-      int node1 = i * N + j;
-      int node2 = ii * N + jj;
-      if (m[node1][node2]) continue;
-
-      locations[2 * k] = node1;
-      locations[2 * k + 1] = node2;
-
-      X[node1] = i;
-      Y[node1] = j;
-      X[node2] = ii;
-      Y[node2] = jj;
-
-      addEdge(graph, node1, node2, i, j, ii, jj);
-      m[node1][node2] = true;
-    }
-
-    System.out.println(graph);
-
-    for (int i = 0; i < 10 * n; i++) {
-
-      int s = locations[RANDOM.nextInt(2 * connections)];
-      int e = locations[RANDOM.nextInt(2 * connections)];
-
-      double d = dijkstra(graph, s, e, n);
-      double a = astar(X, Y, graph, s, e, n);
-      // System.out.println(a + " " + d);
-      if (a != d) {
-        System.out.println("ERROR: " + a + " " + d + " " + s + " " + e);
-        // System.out.println(graph);
-        break;
-      }
-      System.out.println();
-    }
+    System.out.println(
+        "The smallest distance from vertex 0 to vertex 1 is "
+            + aStar(graph, 0, 1)); // Excepted output is 2.
   }
 
-  static void addEdge(
-      Map<Integer, List<Edge>> graph, int f, int t, int fx, int fy, int tx, int ty) {
-    double dx = Math.abs(fx - tx);
-    double dy = Math.abs(fy - ty);
-    graph.get(f).add(new Edge(f, t, dx + dy));
-  }
+  /** An edge in the graph. */
+  public static class Edge {
+    /** The weight of this edge. */
+    int weight;
+    /** The ending vertex of the edge. */
+    int to;
 
-  // Node class to track the nodes to visit while running Dijkstra's
-  private static class DNode implements Comparable<DNode> {
-    int id;
-    double value;
-    private static final double EPS = 1e-7;
-
-    public DNode(int nodeID, double nodeValue) {
-      id = nodeID;
-      value = nodeValue;
+    /**
+     * Creates an edge with a from vertex, to vertex, and a weight.
+     *
+     * @param to The to vertex.
+     * @param weight The weight of the edge.
+     */
+    public Edge(int to, int weight) {
+      this.to = to;
+      this.weight = weight;
     }
-
-    @Override
-    public int compareTo(DNode other) {
-      if (Math.abs(value - other.value) < EPS) return 0;
-      return (value - other.value) > 0 ? +1 : -1;
-    }
-  }
-
-  // Run Dijkstra's algorithm on a directed graph to find the shortest path
-  // from a starting node to an ending node. If there is no path between the
-  // starting node and the destination node the returned value is set to be
-  // Double.POSITIVE_INFINITY.
-  public static double dijkstra(Map<Integer, List<Edge>> graph, int start, int end, int n) {
-
-    // Maintain an array of the minimum distance to each node
-    double[] dists = new double[n];
-    Arrays.fill(dists, Double.POSITIVE_INFINITY);
-    dists[start] = 0;
-
-    // Keep a priority queue of the next most promising node to visit
-    PriorityQueue<DNode> pq = new PriorityQueue<>();
-    pq.offer(new DNode(start, 0.0));
-
-    // Track which nodes have already been visited
-    boolean[] visited = new boolean[n];
-
-    // In the event that you wish to rebuild the shortest path
-    // you can do so using the prev array and starting at some node 'end'
-    // and finding the previous node using prev[end] and the previous node
-    // after that prev[prev[end]] etc... working all the way back until
-    // the index of start is found. Simply uncomment where the prev array is used.
-    // int[] prev = new int[n];
-
-    while (!pq.isEmpty()) {
-
-      DNode node = pq.poll();
-      visited[node.id] = true;
-
-      // We already found a better path before we got to
-      // processing this node so we can ignore it.
-      if (node.value > dists[node.id]) continue;
-
-      List<Edge> edges = graph.get(node.id);
-      if (edges != null) {
-        for (int i = 0; i < edges.size(); i++) {
-          Edge edge = edges.get(i);
-
-          // You cannot get a shorter path by revisiting
-          // a node you have already visited before
-          if (visited[edge.to]) continue;
-
-          // Update minimum cost if applicable
-          double newDist = dists[edge.from] + edge.cost;
-          if (newDist < dists[edge.to]) {
-            // prev[edge.to] = edge.from;
-            dists[edge.to] = newDist;
-            pq.offer(new DNode(edge.to, dists[edge.to]));
-          }
-        }
-      }
-
-      // Once we've visited all the nodes spanning from the end
-      // node we know we can return the minimum distance value to
-      // the end node because it cannot get any better after this point.
-      if (node.id == end) {
-        //         int v = 0;
-        // for (boolean b : visited) if (b) v++;
-        // System.out.println("V: " + v);
-        return dists[end];
-      }
-    }
-
-    // End node is unreachable
-    return Double.POSITIVE_INFINITY;
   }
 }
