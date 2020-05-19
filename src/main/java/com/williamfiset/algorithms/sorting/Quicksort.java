7b43f2f88a2874d90859435d5f2985bbe622b8d4
Nishant Chatterjee <nishantc1527@gmail.com>
2020-05-19 12:02:06 -0700

    reverted sorting classes compile errors

--- src/main/java/com/williamfiset/algorithms/sorting/Quicksort.java
+++ src/main/java/com/williamfiset/algorithms/sorting/Quicksort.java
@@ -11,82 +11,82 @@ package com.williamfiset.algorithms.sorting;
 
 import java.util.Random;
 
-class QuickSort implements InplaceSort {
+public class QuickSort implements InplaceSort {
 
-    @Override
-    public void sort(int[] values) {
-        QuickSort.quicksort(values);
+  @Override
+  public void sort(int[] values) {
+    QuickSort.quicksort(values);
+  }
+
+  public static void quicksort(int[] ar) {
+    if (ar == null) return;
+    quicksort(ar, 0, ar.length - 1);
+  }
+
+  // Sort interval [lo, hi] inplace recursively
+  private static void quicksort(int[] ar, int lo, int hi) {
+    if (lo < hi) {
+      int splitPoint = partition(ar, lo, hi);
+      quicksort(ar, lo, splitPoint);
+      quicksort(ar, splitPoint + 1, hi);
     }
+  }
 
-    public static void quicksort(int[] ar) {
-        if (ar == null) return;
-        quicksort(ar, 0, ar.length - 1);
+  // Performs Hoare partition algorithm for quicksort
+  private static int partition(int[] ar, int lo, int hi) {
+    int pivot = ar[lo];
+    int i = lo - 1, j = hi + 1;
+    while (true) {
+      do {
+        i++;
+      } while (ar[i] < pivot);
+      do {
+        j--;
+      } while (ar[j] > pivot);
+      if (i < j) swap(ar, i, j);
+      else return j;
     }
+  }
 
-    // Sort interval [lo, hi] inplace recursively
-    private static void quicksort(int[] ar, int lo, int hi) {
-        if (lo < hi) {
-            int splitPoint = partition(ar, lo, hi);
-            quicksort(ar, lo, splitPoint);
-            quicksort(ar, splitPoint + 1, hi);
-        }
+  // Swap two elements
+  private static void swap(int[] ar, int i, int j) {
+    int tmp = ar[i];
+    ar[i] = ar[j];
+    ar[j] = tmp;
+  }
+
+  public static void main(String[] args) {
+    InplaceSort sorter = new QuickSort();
+    int[] array = {10, 4, 6, 4, 8, -13, 2, 3};
+    sorter.sort(array);
+    // Prints:
+    // [-13, 2, 3, 4, 4, 6, 8, 10]
+    System.out.println(java.util.Arrays.toString(array));
+
+    // TODO(williamfiset): Move to test file
+    runTests();
+  }
+
+  /* TESTING BELOW */
+
+  static Random RANDOM = new Random();
+
+  public static void runTests() {
+    final int NUM_TESTS = 1000;
+    for (int i = 1; i <= NUM_TESTS; i++) {
+
+      int[] array = new int[i];
+      for (int j = 0; j < i; j++) array[j] = randInt(-1000000, +1000000);
+      int[] arrayCopy = array.clone();
+
+      quicksort(array);
+      java.util.Arrays.sort(arrayCopy);
+
+      if (!java.util.Arrays.equals(array, arrayCopy)) System.out.println("ERROR");
     }
+  }
 
-    // Performs Hoare partition algorithm for quicksort
-    private static int partition(int[] ar, int lo, int hi) {
-        int pivot = ar[lo];
-        int i = lo - 1, j = hi + 1;
-        while (true) {
-            do {
-                i++;
-            } while (ar[i] < pivot);
-            do {
-                j--;
-            } while (ar[j] > pivot);
-            if (i < j) swap(ar, i, j);
-            else return j;
-        }
-    }
-
-    // Swap two elements
-    private static void swap(int[] ar, int i, int j) {
-        int tmp = ar[i];
-        ar[i] = ar[j];
-        ar[j] = tmp;
-    }
-
-    public static void main(String[] args) {
-        InplaceSort sorter = new QuickSort();
-        int[] array = {10, 4, 6, 4, 8, -13, 2, 3};
-        sorter.sort(array);
-        // Prints:
-        // [-13, 2, 3, 4, 4, 6, 8, 10]
-        System.out.println(java.util.Arrays.toString(array));
-
-        // TODO(williamfiset): Move to test file
-        runTests();
-    }
-
-    /* TESTING BELOW */
-
-    static Random RANDOM = new Random();
-
-    public static void runTests() {
-        final int NUM_TESTS = 1000;
-        for (int i = 1; i <= NUM_TESTS; i++) {
-
-            int[] array = new int[i];
-            for (int j = 0; j < i; j++) array[j] = randInt(-1000000, +1000000);
-            int[] arrayCopy = array.clone();
-
-            quicksort(array);
-            java.util.Arrays.sort(arrayCopy);
-
-            if (!java.util.Arrays.equals(array, arrayCopy)) System.out.println("ERROR");
-        }
-    }
-
-    static int randInt(int min, int max) {
-        return RANDOM.nextInt((max - min) + 1) + min;
-    }
+  static int randInt(int min, int max) {
+    return RANDOM.nextInt((max - min) + 1) + min;
+  }
 }
