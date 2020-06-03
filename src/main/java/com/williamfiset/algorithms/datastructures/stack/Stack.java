75ecbea69be1dccd80732273dc49dbcae3bf1fdd
ttskym <liujkon@gmail.com>
2020-06-04 00:22:09 +0800

    add array stack

--- src/main/java/com/williamfiset/algorithms/datastructures/stack/Stack.java
+++ src/main/java/com/williamfiset/algorithms/datastructures/stack/Stack.java
@@ -1,12 +1,19 @@
 package com.williamfiset.algorithms.datastructures.stack;
 
+/**
+ * @author liujingkun
+ */
 public interface Stack<T> {
+  // return the number of elements in the stack
   public int size();
 
+  // return if the stack is empty
   public boolean isEmpty();
 
+  // push the element on the stack
   public void push(T elem);
 
+  // pop the element off the stack
   public T pop();
 
   public T peek();
--- src/main/java/com/williamfiset/algorithms/datastructures/stack/impl/ArrayStack.java
+++ src/main/java/com/williamfiset/algorithms/datastructures/stack/impl/ArrayStack.java
@@ -4,6 +4,9 @@ import com.williamfiset.algorithms.datastructures.stack.Stack;
 import java.util.Arrays;
 import java.util.EmptyStackException;
 
+/**
+ * @author liujingkun
+ */
 public class ArrayStack<T> implements Stack<T> {
   private int capacity;
   private Object[] data;
@@ -34,6 +37,7 @@ public class ArrayStack<T> implements Stack<T> {
     data[size++] = elem;
   }
 
+  // adjust the capacity to store more element
   private void adjustCap() {
     if (capacity == Integer.MAX_VALUE) {
       throw new OutOfMemoryError();
