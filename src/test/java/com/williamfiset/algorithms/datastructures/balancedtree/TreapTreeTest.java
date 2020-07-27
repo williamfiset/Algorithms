1c8784424c3c0a743bccc9205c2bbe8734e56ff3
Jason Chang <j.zisheng@gmail.com>
2020-07-27 18:00:09 -0400

    added skeleton class and skeleton test class

---
+++ src/test/java/com/williamfiset/algorithms/datastructures/balancedtree/TreapTreeTest.java
@@ -0,0 +1,11 @@
+package com.williamfiset.algorithms.datastructures.balancedtree;
+
+import org.junit.Before;
+
+public class TreapTreeTest {
+    private TreapTree<Integer> tree;
+    @Before
+    public void setup(){
+        tree = new TreapTree<>();
+    }
+}
