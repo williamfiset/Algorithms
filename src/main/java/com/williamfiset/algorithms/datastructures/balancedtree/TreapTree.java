4cd91ad8189a0b1d55571a61ebf2b9423ffb08d8
Jason Chang <j.zisheng@gmail.com>
2020-07-29 21:06:01 -0400

    finsihed rotating, and test cases for rotatations for treap

--- src/main/java/com/williamfiset/algorithms/datastructures/balancedtree/TreapTree.java
+++ src/main/java/com/williamfiset/algorithms/datastructures/balancedtree/TreapTree.java
@@ -1,6 +1,7 @@
 /**
  * This file contains an implementation of a Treap Class. Any comparable data
  * is allowed within this tree(numbers, strings, comparable Objects, etc...)
+ * this is a max heap implementation(highest priority at top)
  * Supported operations include:
  * insert(x)
  * remove(x)
@@ -11,26 +12,35 @@
 
 package com.williamfiset.algorithms.datastructures.balancedtree;
 
+import com.williamfiset.algorithms.datastructures.utils.TreePrinter;
+
 import java.util.Random;
 import java.awt.*;
 public class TreapTree<T extends Comparable<T>> {
+
+    static final int MAX_RAND_NUM = 100;
+
     private Random random;
 
-    public class Node {
+    public class Node  implements TreePrinter.PrintableNode  {
+
+
         // The value/data contained within the node
         public T value;
 
         // The int priority of this node for Treap
         public int priority;
 
-        // The left right and parent references of this node
+        // The left and right references of this node
         public Node left, right;
 
-        public Node(T value) {
+        public Node(T value, int priority) {
             this.value = value;
             this.priority = random.nextInt(100);
             this.left = this.right = null;
+            this.priority = priority;
         }
+
         public T getValue() {
             return value;
         }
@@ -39,21 +49,21 @@ public class TreapTree<T extends Comparable<T>> {
             this.value = value;
         }
 
-        public TreapTree.Node getLeft() {
+        @Override
+        public TreePrinter.PrintableNode getLeft() {
             return left;
         }
 
-        public void setLeft(TreapTree.Node left) {
-            this.left = left;
-        }
-
-        public TreapTree.Node getRight() {
+        @Override
+        public TreePrinter.PrintableNode getRight() {
             return right;
         }
 
-        public void setRight(TreapTree.Node right) {
-            this.right = right;
+        @Override
+        public String getText() {
+            return value.toString();
         }
+
     }
     // The root node of the Treap tree.
     public Node root;
@@ -61,15 +71,10 @@ public class TreapTree<T extends Comparable<T>> {
     // Tracks the number of nodes inside the tree
     private int nodeCount = 0;
 
-    public final Node NIL;
-
-    public TreapTree(){
-        NIL = new Node(null);
-        NIL.left = null;
-        NIL.right = null;
-
-        root = NIL;
+    public TreapTree() {
+        random = new Random();
     }
+
     // returns the number of nodes in the tree
     public int size(){
         return this.nodeCount;
@@ -79,14 +84,14 @@ public class TreapTree<T extends Comparable<T>> {
         return contains(root, value);
     }
 
-    private boolean contains(Node node, T elem) {
-        if (node == null) return false;
+    private boolean contains(Node node, T value) {
+        if (node == null || value == null) return false;
 
-        int cmp = elem.compareTo(node.getValue());
+        int cmp = value.compareTo(node.getValue());
 
-        if (cmp < 0) return contains(node.left, elem);
+        if (cmp < 0) return contains(node.left, value);
 
-        else if (cmp > 0) return contains(node.right, elem);
+        else if (cmp > 0) return contains(node.right, value);
 
         else return true;
     }
@@ -95,28 +100,71 @@ public class TreapTree<T extends Comparable<T>> {
         return (nodeCount == 0);
     }
 
-    public boolean insert(T val){
+    public boolean insert(T val, int priority){
         if (val == null){
             throw new IllegalArgumentException("TreapTree does not allow null values");
         }
+        if(!contains(root, val)) {
+            root = insert(this.root, val, priority);
+            nodeCount++;
+            return true;
+        }
+        return false;
     }
 
-    private Node insert(Node node, T value) {
+    public boolean insert(T val){
+        return insert(val, random.nextInt(MAX_RAND_NUM));
+    }
+
+    private Node insert(Node node, T value, int priority) {
         // base case
-        if (node == null) return new Node(value);
+        if (node == null) { return new Node(value, priority); }
 
         // compare curr val to the val in the node
         int compareResult = value.compareTo(node.value);
 
         if (compareResult < 0){
-            node.left = insert(node.left, value);
-            if (node.left.priority < node.priority){
-                // rotate with left child (node)
+            node.left = insert(node.left, value, priority);
+            if (node.left.priority > node.priority){
+                // right rotation
+                node = rightRotation(node);
             }
         }
+        else if (compareResult > 0){
+            node.right = insert(node.right, value, priority);
+            if (node.right.priority > node.priority){
+                // left rotation
+                node = leftRotation(node);
+            }
+        }
+        // is duplicate, return nothing
+        return node;
     }
 
-    public boolean delete(T key){
-        return true;
+    private Node leftRotation(Node node){
+        Node newParent = node.right;
+        node.right = newParent.left;
+        newParent.left = node;
+        return newParent;
     }
+
+
+    private Node rightRotation(Node node) {
+        Node newParent = node.left;
+        node.left = newParent.right;
+        newParent.right = node;
+        return newParent;
+    }
+
+    public void remove(T elem){
+        root = remove(root, elem);
+    }
+
+    public Node remove(Node node, T elem){
+        if (node == null) {
+            return node;
+        }
+        int cmp = elem.compareTo(node.value);
+    }
+
 }
--- src/test/java/com/williamfiset/algorithms/datastructures/balancedtree/TreapTreeTest.java
+++ src/test/java/com/williamfiset/algorithms/datastructures/balancedtree/TreapTreeTest.java
@@ -1,11 +1,138 @@
 package com.williamfiset.algorithms.datastructures.balancedtree;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.TreeSet;
+
+import static com.google.common.truth.Truth.assertThat;
 
 public class TreapTreeTest {
-    private TreapTree<Integer> tree;
-    @Before
-    public void setup(){
-        tree = new TreapTree<>();
+
+  static final int MAX_RAND_NUM = +100000;
+  static final int MIN_RAND_NUM = -100000;
+
+  static final int TEST_SZ = 9000;
+
+  private TreapTree<Integer> tree;
+
+  @Before
+  public void setup(){
+    tree = new TreapTree<>();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testNullInsertion(){
+    tree.insert(null);
+  }
+
+  @Test
+  public void testTreeContainsNull(){
+    assertThat(tree.contains(null)).isFalse();
+  }
+
+  @Test
+  public void LeftLeftCase() {
+    tree.insert(15, 15);
+    tree.insert(10,8);
+    tree.insert(20,10);
+    tree.insert(30,9);
+
+    assertThat(tree.root.left.getValue()).isEqualTo(10);
+    assertThat(tree.root.getValue()).isEqualTo(15);
+    assertThat(tree.root.right.getValue()).isEqualTo(20);
+    assertThat(tree.root.right.right.getValue()).isEqualTo(30);
+
+    tree.insert(32,14);
+
+    assertThat(tree.root.left.getValue()).isEqualTo(10);
+    assertThat(tree.root.getValue()).isEqualTo(15);
+    assertThat(tree.root.right.getValue()).isEqualTo(32);
+    assertThat(tree.root.right.left.getValue()).isEqualTo(20);
+    assertThat(tree.root.right.left.right.getValue()).isEqualTo(30);
+
+    assertThat(tree.root.right.left.right.left).isNull();
+    assertThat(tree.root.right.left.right.right).isNull();
+    assertThat(tree.root.right.left.left).isNull();
+    assertThat(tree.root.right.right).isNull();
+    assertThat(tree.root.left.left).isNull();
+    assertThat(tree.root.left.right).isNull();
+
+  }
+
+  @Test
+  public void testLeftRightCase() {
+    tree.insert(20, 10);
+    tree.insert(17,5);
+    tree.insert(26,7);
+
+    assertThat(tree.root.getValue()).isEqualTo(20);
+    assertThat(tree.root.left.getValue()).isEqualTo(17);
+    assertThat(tree.root.right.getValue()).isEqualTo(26);
+
+    tree.insert(18,15);
+    assertThat(tree.root.getValue()).isEqualTo(18);
+    assertThat(tree.root.left.getValue()).isEqualTo(17);
+    assertThat(tree.root.right.getValue()).isEqualTo(20);
+    assertThat(tree.root.right.right.getValue()).isEqualTo(26);
+
+    assertThat(tree.root.left.left).isNull();
+    assertThat(tree.root.left.right).isNull();
+    assertThat(tree.root.right.left).isNull();
+    assertThat(tree.root.right.right.left).isNull();
+    assertThat(tree.root.right.right.right).isNull();
+  }
+
+  @Test
+  public void testRightRightCase() {
+
+  }
+
+  @Test
+  public void testRightLeftCase() {
+
+  }
+
+  @Test
+  public void randomRemoveTests() {
+    /*
+    TreeSet<Integer> ts = new TreeSet<>();
+    for (int i = 0; i < TEST_SZ; i++) {
+
+      int size = i;
+      List<Integer> lst = genRandList(size);
+      for (Integer value : lst) {
+        tree.insert(value);
+        ts.add(value);
+      }
+      Collections.shuffle(lst);
+
+      // Remove all the elements we just placed in the tree.
+      for (int j = 0; j < size; j++) {
+
+        Integer value = lst.get(j);
+
+        assertThat(tree.remove(value)).isEqualTo(ts.remove(value));
+        assertThat(tree.contains(value)).isFalse();
+        assertThat(tree.size()).isEqualTo(size - j - 1);
+      }
+
+      assertThat(tree.isEmpty()).isTrue();
     }
+     */
+  }
+
+  static List<Integer> genRandList(int sz) {
+    List<Integer> lst = new ArrayList<>(sz);
+    for (int i = 0; i < sz; i++) lst.add(i); // unique values.
+    Collections.shuffle(lst);
+    return lst;
+  }
+
+  public static int randValue() {
+    return (int) (Math.random() * MAX_RAND_NUM * 2) + MIN_RAND_NUM;
+  }
 }
