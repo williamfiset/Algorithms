b8bf26f6face46300f22a857df8f8030b05127cd
Sohel Shaikh <sohelshaikh97@gmail.com>
2020-10-22 16:30:57 +0530

    Added search method
    Searches and returns the index of the element if present in the stack

--- src/main/java/com/williamfiset/algorithms/datastructures/stack/ListStack.java
+++ src/main/java/com/williamfiset/algorithms/datastructures/stack/ListStack.java
@@ -45,6 +45,12 @@ public class ListStack<T> implements Iterable<T>, Stack<T> {
     if (isEmpty()) throw new java.util.EmptyStackException();
     return list.peekLast();
   }
+  
+  // Searches for the element starting from top of the stack
+  // Returns -1 if the element is not present in the stack
+  public int search(T elem) {
+    return list.lastIndexOf(elem);
+  }
 
   // Allow users to iterate through the stack using an iterator
   @Override
