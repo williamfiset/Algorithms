04fa0e9852246d32285e4d3d4adb5d6b4248e4da
murali <muralie39@gmail.com>
2020-11-02 20:15:14 -0500

    code style verified

--- src/main/java/com/williamfiset/algorithms/math/LargestPrimeFactor.java
+++ src/main/java/com/williamfiset/algorithms/math/LargestPrimeFactor.java
@@ -2,24 +2,23 @@ package com.williamfiset.algorithms.math;
 
 public class LargestPrimeFactor {
 
-    static long maxPrime( long val){
-        long max_prime = -1;
-        while (val % 2 == 0) {
-            max_prime = 2;
-            val = 1;
-        }
-        for (int i = 3; i <= Math.sqrt(val); i += 2){
-            while (val % i == 0){
-                max_prime = i;
-                val = val / i;
-            }
-        }
-        if (val > 2)
-            max_prime = val;
-        return max_prime;
+  static long maxPrime(long val) {
+    long max_prime = -1;
+    while (val % 2 == 0) {
+      max_prime = 2;
+      val = 1;
     }
+    for (int i = 3; i <= Math.sqrt(val); i += 2) {
+      while (val % i == 0) {
+        max_prime = i;
+        val = val / i;
+      }
+    }
+    if (val > 2) max_prime = val;
+    return max_prime;
+  }
 
-    public static void main(String[] args) {
-        System.out.println(maxPrime(600851475143L));
-    }
+  public static void main(String[] args) {
+    System.out.println(maxPrime(600851475143L));
+  }
 }
