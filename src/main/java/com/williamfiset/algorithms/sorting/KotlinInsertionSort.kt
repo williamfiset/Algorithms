cd99b006c5a105e950be8e62e1008e5f4b5e9039
Gabe Pesco <gabepesco@gmail.com>
2020-12-02 17:39:40 -0500

    adding insertion sort with kotlin using intellij's conversion

--- src/main/java/com/williamfiset/algorithms/sorting/KotlinInsertionSort.kt
+++ src/main/java/com/williamfiset/algorithms/sorting/KotlinInsertionSort.kt
@@ -1,18 +1,41 @@
 package com.williamfiset.algorithms.sorting
 
-fun insertionsort(list:MutableList<Int>):List<Int> {
-    if (list.isEmpty() || list.size < 2){
-        return list
-    }
-    for (i in 1..list.size - 1){
-        val item = list[i]
-        var j = i
-        while (j > 0 && item < list[j - 1]){
-            list[j] = list[j - 1]
-            j -= 1
-        }
-        list[j] = item
-    }
-    return list
+import java.util.*
+import kotlin.jvm.JvmStatic
 
+class KotlinInsertionSort : InplaceSort {
+    override fun sort(values: IntArray) {
+        insertionSort(values)
+    }
+
+    companion object {
+        private fun insertionSort(ar: IntArray?) {
+            if (ar == null) {
+                return
+            }
+            for (i in 1 until ar.size) {
+                var j = i
+                while (j > 0 && ar[j] < ar[j - 1]) {
+                    swap(ar, j - 1, j)
+                    j--
+                }
+            }
+        }
+
+        private fun swap(ar: IntArray, i: Int, j: Int) {
+            val tmp = ar[i]
+            ar[i] = ar[j]
+            ar[j] = tmp
+        }
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val sorter: InplaceSort = InsertionSort()
+            val array = intArrayOf(10, 4, 6, 8, 13, 2, 3)
+            sorter.sort(array)
+            // Prints:
+            // [2, 3, 4, 6, 8, 10, 13]
+            println(Arrays.toString(array))
+        }
+    }
 }
