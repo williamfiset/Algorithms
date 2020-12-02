d1dca49e9437d86368c385fe41aedaddb42f3d6e
JoshHayden <jhayden@ithaca.edu>
2020-12-02 16:53:57 -0500

    Implementation of KotlinInsertionSort added, needs testing

--- src/main/java/com/williamfiset/algorithms/sorting/KotlinInsertionSort.kt
+++ src/main/java/com/williamfiset/algorithms/sorting/KotlinInsertionSort.kt
@@ -1,2 +1,18 @@
 package com.williamfiset.algorithms.sorting
 
+fun insertionsort(list:MutableList<Int>):List<Int> {
+    if (list.isEmpty() || list.size < 2){
+        return list
+    }
+    for (i in 1..list.size - 1){
+        val item = list[i]
+        var j = i
+        while (j > 0 && item < list[j - 1]){
+            list[j] = list[j - 1]
+            j -= 1
+        }
+        list[j] = item
+    }
+    return list
+
+}
