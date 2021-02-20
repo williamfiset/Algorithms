f1a56eca79201075752415cd6dee83369e96a41e
Eli Jagnow <eli.jagnow@gmail.com>
2021-02-19 19:32:46 -0600

    Tree Creation
    The basic structure of the k-dimension tree has been implemented, with stand-in methods for insert, search, findMin, and delete.

--- src/main/java/com/williamfiset/algorithms/datastructures/kdtree/GeneralKDTree.java
+++ src/main/java/com/williamfiset/algorithms/datastructures/kdtree/GeneralKDTree.java
@@ -1,5 +1,51 @@
+/**
+ * A generic k-dimensional tree implementation.
+ * 
+ * @author David Jagnow
+ */
+
 package com.williamfiset.algorithms.datastructures.kdtree;
 
 public class GeneralKDTree {
     
+    private int k;
+    private KDNode root;
+    
+    /* Definition of the tree */
+    public GeneralKDTree(int dimensions) {
+        k = dimensions;
+        root = null;
+    }
+
+    /* Tree Methods */
+    public void insert(int[] toAdd) {
+        //?
+    }
+
+    public void search(int[] element) {
+        //?
+    }
+
+    public int[] findMin(int dim) {
+        return null;
+    }
+
+    public int[] delete(int[] toRemove) {
+        return null;
+    }
+
+    /* Definition of the tree nodes */
+    private class KDNode {
+
+        private int[] point;
+        private KDNode left;
+        private KDNode right;
+
+        public KDNode(int[] coords) {
+            if(coords.length != k) throw new IllegalArgumentException("Error: Expected " + k + "dimensions, but given " + coords.length);
+            point = coords;
+            left = null;
+            right = null;
+        }
+    }
 }
