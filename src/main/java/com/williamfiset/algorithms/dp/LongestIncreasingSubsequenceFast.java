a5f57144dcba96aeaf492a908182401163713f41
Ioana Cotutiu <ioanacotutiu31@gmail.com>
2021-03-03 17:38:10 +0200

    refactor longestIncreasingSubsequenceFast
    Added code documentation.
    Renamed variables.
    Refactored code.
    
    Co-Authored-By: Theodor Andrei Moise <38431187+iriediese@users.noreply.github.com>

--- src/main/java/com/williamfiset/algorithms/dp/LongestIncreasingSubsequenceFast.java
+++ src/main/java/com/williamfiset/algorithms/dp/LongestIncreasingSubsequenceFast.java
@@ -1,63 +1,67 @@
 package com.williamfiset.algorithms.dp;
 
+/**
+ * Finds the longest increasing subsequence within an array of numbers
+ * obtained by removing the fewest possible elements from the initial array.
+ *
+ * Complexity: O(n * log n)
+ */
 public class LongestIncreasingSubsequenceFast {
   public static void main(String[] args) {
 
-    System.out.println(lis(new int[] {1, 3, 2, 4, 3})); // 3
-    System.out.println(lis(new int[] {2, 7, 4, 3, 8})); // 3
-    System.out.println(lis(new int[] {5, 4, 3, 2, 1})); // 1
-    System.out.println(lis(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9})); // 9
-    System.out.println(lis(new int[] {0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15})); // 6
+    System.out.println(longestIncreasingSubsequenceLength(new int[] {1, 3, 2, 4, 3})); // 3
+    System.out.println(longestIncreasingSubsequenceLength(new int[] {2, 7, 4, 3, 8})); // 3
+    System.out.println(longestIncreasingSubsequenceLength(new int[] {5, 4, 3, 2, 1})); // 1
+    System.out.println(
+            longestIncreasingSubsequenceLength(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9})); // 9
+    System.out.println(
+            longestIncreasingSubsequenceLength(
+                    new int[] {0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15})); // 6
   }
 
-  public static int binarySearch(int[] ar, int[] c, int i, int sz) {
-    int mid, lo = 1, hi = sz;
-    while (lo <= hi) {
-      mid = (int) ((lo + hi) / 2);
-      if (c[mid] < ar[i]) {
-        lo = mid + 1;
-      }	else {
-        hi = mid - 1;
-      }
-    }
-    //System.out.println("bs " + lo);
-    return lo;
-  }
-
-  // Finds the length of the longest increasing subsequence length, O(n^2)
-  public static int lis(int[] ar) {
-
-    if (ar == null || ar.length == 0) return 0;
-    int n = ar.length, len = 0, sz = 1;
-
-    // When starting, each individual element has a LIS
-    // of exactly one, so each index is initialized to 1
-    int[] dp = new int[n];
-    int[] c = new int[n + 1];
-    c[1] = ar[0];
-    dp[0] = 1;
-    java.util.Arrays.fill(dp, 1);
-    java.util.Arrays.fill(c, ar[0]);
-
-    // Processing the array left to right update the value of dp[j] if two
-    // conditions hold 1) The value at i is less than that of the one at j
-    // and 2) updating the value of dp[j] to dp[i]+1 is better
-    for (int i = 1; i < n; i++) {
-      if (ar[i] < c[1]) {
-        c[1] = ar[i]; /*you have to update the minimum value right now*/
-        dp[i] = 1;
-      } else if (ar[i] > c[sz]) {
-        c[sz + 1] = ar[i];
-        dp[i] = sz + 1;
-        sz++;
+  /**
+   * 
+   * @param ar parsed array
+   * @param l lower bound representing the leftmost index of the array
+   * @param r upper bound representing the rightmost index of the array
+   * @param key value to be inserted
+   * @return index before which the key can be inserted
+   */
+  static int binarySearch(int[] ar, int l, int r, int key) {
+    while (r - l > 1) {
+      int m = l + (r - l) / 2;
+      if (ar[m] >= key) {
+        r = m;
       } else {
-        int k = binarySearch(ar, c, i, sz); /*you want to find k so that c[k-1]<arr[i]<c[k]*/
-        c[k] = ar[i];
-        dp[i] = k;
+        l = m;
       }
-      if (dp[i] > len) len = dp[i];
     }
+    return r;
+  }
 
+  /**
+   *
+   * @param ar initial array
+   * @return the length of the longest increasing subsequence
+   */
+  static int longestIncreasingSubsequenceLength(int[] ar) {
+    int size = ar.length;
+    int[] lis = new int[size];
+    int len;
+    lis[0] = ar[0];
+    len = 1;
+    for (int i = 1; i < size; i++) {
+      // minimum value needs updating
+      if (ar[i] < lis[0]) {
+        lis[0] = ar[i];
+      }
+      // new value can be safely appended
+      else if (ar[i] > lis[len - 1]) {
+        lis[len++] = ar[i];
+      } else { // position needs to be found for new value
+        lis[binarySearch(lis, -1, len - 1, ar[i])] = ar[i];
+      }
+    }
     return len;
   }
 }
