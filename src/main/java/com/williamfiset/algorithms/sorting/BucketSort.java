be4b0a9533de73461261e9aa6aca35b9e0c50f41
Tianyi Shi <ShiTianyi2001@outlook.com>
2021-06-22 10:19:52 +0100

    to demonstrate that all elements are placed into bucket 0

--- src/main/java/com/williamfiset/algorithms/sorting/BucketSort.java
+++ src/main/java/com/williamfiset/algorithms/sorting/BucketSort.java
@@ -38,6 +38,7 @@ public class BucketSort implements InplaceSort {
     // Place each element in a bucket
     for (int i = 0; i < N; i++) {
       int bi = (ar[i] - minValue) / M;
+      System.out.printf("%d placed into bucket %d\n", ar[i], bi);
       List<Integer> bucket = buckets.get(bi);
       bucket.add(ar[i]);
     }
