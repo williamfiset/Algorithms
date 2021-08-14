2eed08cd0de39ce73d445e83e6aa1476741edc51
William Fiset <williamfiset@Williams-Mac-mini.local>
2021-08-14 11:40:42 -0700

    formatting coin change

--- src/main/java/com/williamfiset/algorithms/dp/CoinChange.java
+++ src/main/java/com/williamfiset/algorithms/dp/CoinChange.java
@@ -20,9 +20,9 @@ public class CoinChange {
 
     final int N = coins.length;
     // Initialize table and set first row to be infinity
-    int[][] DP = new int[N + 1][amount + 1];
-    java.util.Arrays.fill(DP[0], INF);
-    DP[1][0] = 0;
+    int[][] dp = new int[N + 1][amount + 1];
+    java.util.Arrays.fill(dp[0], INF);
+    dp[1][0] = 0;
 
     // Iterate through all the coins
     for (int i = 1; i <= N; i++) {
@@ -31,19 +31,19 @@ public class CoinChange {
       for (int j = 1; j <= amount; j++) {
 
         // Consider not selecting this coin
-        DP[i][j] = DP[i - 1][j];
+        dp[i][j] = dp[i - 1][j];
 
         // Try selecting this coin if it's better
-        if (j - coinValue >= 0 && DP[i][j - coinValue] + 1 < DP[i][j])
-          DP[i][j] = DP[i][j - coinValue] + 1;
+        if (j - coinValue >= 0 && dp[i][j - coinValue] + 1 < dp[i][j])
+          dp[i][j] = dp[i][j - coinValue] + 1;
       }
     }
 
     // The amount we wanted to make cannot be made :/
-    if (DP[N][amount] == INF) return -1;
+    if (dp[N][amount] == INF) return -1;
 
     // Return the minimum number of coins needed
-    return DP[N][amount];
+    return dp[N][amount];
   }
 
   public static int coinChangeSpaceEfficient(int[] coins, int amount) {
@@ -51,19 +51,23 @@ public class CoinChange {
     if (coins == null) throw new IllegalArgumentException("Coins array is null");
 
     // Initialize table and set everything to infinity except first cell
-    int[] DP = new int[amount + 1];
-    java.util.Arrays.fill(DP, INF);
-    DP[0] = 0;
+    int[] dp = new int[amount + 1];
+    java.util.Arrays.fill(dp, INF);
+    dp[0] = 0;
 
-    for (int i = 1; i <= amount; i++)
-      for (int coinValue : coins)
-        if (i - coinValue >= 0 && DP[i - coinValue] + 1 < DP[i]) DP[i] = DP[i - coinValue] + 1;
+    for (int i = 1; i <= amount; i++) {
+      for (int coinValue : coins) {
+        if (i - coinValue >= 0 && dp[i - coinValue] + 1 < dp[i]) {
+          dp[i] = dp[i - coinValue] + 1;
+        }
+      }
+    }
 
     // The amount we wanted to make cannot be made :/
-    if (DP[amount] == INF) return -1;
+    if (dp[amount] == INF) return -1;
 
     // Return the minimum number of coins needed
-    return DP[amount];
+    return dp[amount];
   }
 
   // The recursive approach has the advantage that it does not have to visit
@@ -74,29 +78,29 @@ public class CoinChange {
     if (coins == null) throw new IllegalArgumentException("Coins array is null");
     if (amount < 0) return -1;
 
-    int[] DP = new int[amount + 1];
-    return coinChangeRecursive(amount, coins, DP);
+    int[] dp = new int[amount + 1];
+    return coinChangeRecursive(amount, coins, dp);
   }
 
   // Private helper method to actually go the recursion
-  private static int coinChangeRecursive(int amount, int[] coins, int[] DP) {
+  private static int coinChangeRecursive(int amount, int[] coins, int[] dp) {
 
     // Base cases.
     if (amount < 0) return -1;
     if (amount == 0) return 0;
-    if (DP[amount] != 0) return DP[amount];
+    if (dp[amount] != 0) return dp[amount];
 
     int minCoins = INF;
     for (int coinValue : coins) {
 
       int newAmount = amount - coinValue;
-      int value = coinChangeRecursive(newAmount, coins, DP);
+      int value = coinChangeRecursive(newAmount, coins, dp);
       if (value != -1 && value < minCoins) minCoins = value + 1;
     }
 
     // If we weren't able to find some coins to make our
     // amount then cache -1 as the answer.
-    return DP[amount] = (minCoins == INF) ? -1 : minCoins;
+    return dp[amount] = (minCoins == INF) ? -1 : minCoins;
   }
 
   public static void main(String[] args) {
