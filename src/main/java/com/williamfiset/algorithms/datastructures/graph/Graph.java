932528f3fe615f07bd6caa614576dd9597967b12
Jazmine <jazmine@kth.se>
2022-03-02 22:05:29 +0100

    Feature: Function to remove nodes and edges implemented
    ArrayList of nodes representing the graph changed to a HashMap due to a quicker and simpler checkout
    of specific nodes. Removing a node removes all edges connected to the node. Removing an edge removes
    it from both nodes it connects.
    
    Fixes #2

--- src/main/java/com/williamfiset/algorithms/datastructures/graph/Graph.java
+++ src/main/java/com/williamfiset/algorithms/datastructures/graph/Graph.java
@@ -7,18 +7,20 @@
 package com.williamfiset.algorithms.datastructures.graph;
 
 import java.util.ArrayList;
+import java.util.ListIterator;
 import java.util.List;
+import java.util.HashMap;
 
 public class Graph {
   
-  private List<Node> graph;
+  private HashMap<Integer, Node> graph;
   
   /** 
   * An edge class that represents a directed edge between two nodes
   */
   public static class Edge {
-	int from;
-	int to;
+	Integer from;
+	Integer to;
 	
 	/**
     * Initiates a single edge between the two nodes specified
@@ -26,7 +28,7 @@ public class Graph {
 	* @param from - if of the node the edge comes from
 	* @param to - The id of the the edge comes to
     */
-	public Edge(int from, int to) {
+	public Edge(Integer from, Integer to) {
       this.from = from;
       this.to = to;
     }
@@ -39,16 +41,17 @@ public class Graph {
   *
   */
   public static class Node {
-    int id;
-    List<Edge> edges;
+    Integer id;
+    private List<Edge> edges;
 
 	/**
 	* Initiates a single node with a specified id
 	*
 	* @param id - The id of the node initiated
     */
-    public Node(int id) {
+    public Node(Integer id) {
       this.id = id;
+	  edges = new ArrayList<Edge>();
     }
 	
 	/**
@@ -69,45 +72,68 @@ public class Graph {
 	}
 	
 	/**
+	* Returns the list of edges
+    */
+	public List<Edge> getEdges() {
+		return edges;
+	}
+	
+	/**
 	* Adds a specified edge to the list of edges
 	*
 	* @param edge - The instance of Edge to add to list of edges
     */
 	private void addEdge(Edge edge) {
-		this.edges.add(edge);
+		edges.add(edge);
 	}
 	
 	/**
-	* Returns the list of edges
+	* Removes all edges connecting to a specified node
+	* If there are multiple edges towards the same node they will all be removed
+	*
+	* @param id - The id of the node the edge connects to
     */
-	private List<Edge> getEdges() {
-		return edges;
+	public void removeEdge(Integer idTo) {
+      ListIterator<Edge> iterator = edges.listIterator();
+	  
+	  while(iterator.hasNext()){
+		if(iterator.next().to == idTo){
+		  iterator.remove();
+		}
+	  }
 	}
+	
+	/**
+	* Removes all edges from the node
+    */
+	public void removeAllEdges(){
+	  edges.clear();
+	}
+	
   }
   
   /**
   * Creates a new Graph instance
   */
   public Graph(){
-	  
-	  
+	graph = new HashMap<>();
   }
   
   /**
-  * Returns the list of nodes
+  * Returns the nodes of the graph as a hashmap
   */
-  public List<Node> getGraph(){  
-	return graph;
+  public HashMap<Integer, Node> getGraph(){ 
+	return this.graph;
 	  
   }
   
   /**
-  * Returns the list of edges for a specific node
+  * Returns the node of the specified id
   * 
-  * @param id - The id of the node to get the edges for
+  * @param id - The id of the node to return
   */
-  public List<Edge> getEdges(int id){  
-	return graph.get(id).getEdges();
+  public Node getNode(Integer id){  
+	return graph.get(id);
 	  
   }
   
@@ -116,11 +142,35 @@ public class Graph {
   * 
   * @param id - The id of the node to add
   */
-  public void addNode(int id){  
+  public void addNode(Integer id){  
 	Node node = new Node(id);
 	
-	// Add the new node to the list at the index of the id of the node
-	graph.add(id, node);
+	// Add the new node to the list
+	graph.putIfAbsent(id, node);
+	  
+  }
+  
+  /**
+  * Removes the node specified by the id
+  * Alo removes all edges connected to the node
+  * 
+  * @param id - The id of the node to remove
+  */
+  public void removeNode(Integer id){
+	Node node = getNode(id);
+	List<Edge> edges = node.getEdges();
+	
+	// Itterate over all edges and remove the duplicate ones going to the node being removed
+	for(Edge edge : edges){
+	  Node tempNode = getNode(edge.to);
+	  tempNode.removeEdge(id);
+	}
+	
+	// Remove all edges from the node
+	node.removeAllEdges();
+	
+	// Remove the node once done
+	graph.remove(id);
 	  
   }
   
@@ -130,12 +180,29 @@ public class Graph {
   * @param id1 - The id of the first node
   * @param id2 - The id of the second node
   */
-  public void addEdge(int id1, int id2){  
+  public void addEdge(Integer id1, Integer id2){  
 	Node node1 = graph.get(id1);
 	Node node2 = graph.get(id2);
 	
 	// Add the new edge between the two nodes
 	node1.addNewEdge(node2);
+	
+  }
+  
+  /**
+  * Removes the edge from a specified node that connects the node to another specified one
+  * 
+  * @param idFrom - The id of the node the edge starts at
+  * @param idTo - The id of the node the edge connects to
+  */
+  public void removeEdge(Integer idFrom, Integer idTo){
+	Node nodeFrom = getNode(idFrom);
+	Node nodeTo = getNode(idTo);
+	
+	//Removes both directions of the edge
+	nodeFrom.removeEdge(idTo);
+	nodeTo.removeEdge(idFrom);
+	
 	  
   }
 
