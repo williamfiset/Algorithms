3df96521705986e5e40bc5075fde000816630aad
Johan <jekbe@kth.se>
2022-03-03 15:09:33 +0100

    Finishes tests for bidrectional search
    Now is implemented with the given graph data structure

--- src/test/java/com/williamfiset/algorithms/graphtheory/BidirectionalSearchTest.java
+++ src/test/java/com/williamfiset/algorithms/graphtheory/BidirectionalSearchTest.java
@@ -1,22 +1,14 @@
 package com.williamfiset.algorithms.graphtheory;
 
-import static com.google.common.truth.Truth.assertThat;
-import static com.williamfiset.algorithms.graphtheory.BidirectionialSearch.Edge;
-import static com.williamfiset.algorithms.graphtheory.BidirectionialSearch.addUnweightedUndirectedEdge;
-import static com.williamfiset.algorithms.graphtheory.BidirectionialSearch.createEmptyGraph;
-import static java.lang.Math.max;
-import static java.lang.Math.random;
+import com.williamfiset.algorithms.datastructures.graph.Graph;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
-
-import org.junit.Before;
 import org.junit.Test;
 
 public class BidirectionalSearchTest {
@@ -24,26 +16,28 @@ public class BidirectionalSearchTest {
     @Test
     public void testGraphWithPath(){
         final int n = 15;
-        List<List<Edge>> graph = createEmptyGraph(n);
-
-        addUnweightedUndirectedEdge(graph, 0, 4);
-        addUnweightedUndirectedEdge(graph, 1, 4);
-        addUnweightedUndirectedEdge(graph, 2, 5);
-        addUnweightedUndirectedEdge(graph, 3, 5);
-        addUnweightedUndirectedEdge(graph, 4, 6);
-        addUnweightedUndirectedEdge(graph, 5, 6);
-        addUnweightedUndirectedEdge(graph, 6, 7);
-        addUnweightedUndirectedEdge(graph, 7, 8);
-        addUnweightedUndirectedEdge(graph, 8, 9);
-        addUnweightedUndirectedEdge(graph, 9, 11);
-        addUnweightedUndirectedEdge(graph, 9, 12);
-        addUnweightedUndirectedEdge(graph, 8, 10);
-        addUnweightedUndirectedEdge(graph, 10, 13);
-        addUnweightedUndirectedEdge(graph, 10, 14);
+        Graph g = new Graph();
+        for (int i = 0; i < n; i++) {
+            g.addNode(i);
+        }
+        g.addEdge(0, 4);
+        g.addEdge(1, 4);
+        g.addEdge(2, 5);
+        g.addEdge(3, 5);
+        g.addEdge(4, 6);
+        g.addEdge(5, 6);
+        g.addEdge(6, 7);
+        g.addEdge(7, 8);
+        g.addEdge(8, 9);
+        g.addEdge(9, 11);
+        g.addEdge(9, 12);
+        g.addEdge(8, 10);
+        g.addEdge(10, 13);
+        g.addEdge(10, 14);
         int src = 0;
         int dest = 14;
 
-        BidirectionialSearch solver = new BidirectionialSearch(graph, n);
+        BidirectionialSearch solver = new BidirectionialSearch(g, n);
         List<Integer> actualPath = solver.bidirectionalSearch(src, dest);
         if(actualPath == null)
             fail();
@@ -61,12 +55,15 @@ public class BidirectionalSearchTest {
     @Test
     public void testBFSForward(){
         final int n = 5;
-        List<List<Edge>> graph = createEmptyGraph(n);
-        addUnweightedUndirectedEdge(graph, 0, 1);
-        addUnweightedUndirectedEdge(graph, 0, 2);
-        addUnweightedUndirectedEdge(graph, 0, 3);
-        addUnweightedUndirectedEdge(graph, 0, 4);
-        BidirectionialSearch bds = new BidirectionialSearch(graph, n);
+        Graph g = new Graph();
+        for (int i = 0; i < n; i++) {
+            g.addNode(i);
+        }
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(0, 3);
+        g.addEdge(0, 4);
+        BidirectionialSearch bds = new BidirectionialSearch(g, n);
         bds.srcQueue.offer(0);
         bds.srcVisited[0] = true;
         bds.bfs("forward");
@@ -75,21 +72,24 @@ public class BidirectionalSearchTest {
         assertTrue(bds.srcVisited[3]);
         assertTrue(bds.srcVisited[4]);
 
-        assertEquals(Integer.valueOf(0), bds.srcParent.get(1));
-        assertEquals(Integer.valueOf(0), bds.srcParent.get(2));
-        assertEquals(Integer.valueOf(0), bds.srcParent.get(3));
-        assertEquals(Integer.valueOf(0), bds.srcParent.get(4));
+        assertEquals(Integer.valueOf(0), Integer.valueOf(bds.srcParent[1]));
+        assertEquals(Integer.valueOf(0), Integer.valueOf(bds.srcParent[2]));
+        assertEquals(Integer.valueOf(0), Integer.valueOf(bds.srcParent[3]));
+        assertEquals(Integer.valueOf(0), Integer.valueOf(bds.srcParent[4]));
     }
 
     @Test
     public void testBFSBackward(){
         final int n = 5;
-        List<List<Edge>> graph = createEmptyGraph(n);
-        addUnweightedUndirectedEdge(graph, 0, 1);
-        addUnweightedUndirectedEdge(graph, 0, 2);
-        addUnweightedUndirectedEdge(graph, 0, 3);
-        addUnweightedUndirectedEdge(graph, 0, 4);
-        BidirectionialSearch bds = new BidirectionialSearch(graph, n);
+        Graph g = new Graph();
+        for (int i = 0; i < n; i++) {
+            g.addNode(i);
+        }
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(0, 3);
+        g.addEdge(0, 4);
+        BidirectionialSearch bds = new BidirectionialSearch(g, n);
         bds.destQueue.offer(0);
         bds.destVisited[0] = true;
         bds.bfs("backward");
@@ -98,28 +98,30 @@ public class BidirectionalSearchTest {
         assertTrue(bds.destVisited[3]);
         assertTrue(bds.destVisited[4]);
 
-        assertEquals(Integer.valueOf(0), bds.destParent.get(1));
-        assertEquals(Integer.valueOf(0), bds.destParent.get(2));
-        assertEquals(Integer.valueOf(0), bds.destParent.get(3));
-        assertEquals(Integer.valueOf(0), bds.destParent.get(4));
+        assertEquals(Integer.valueOf(0), Integer.valueOf(bds.destParent[1]));
+        assertEquals(Integer.valueOf(0), Integer.valueOf(bds.destParent[2]));
+        assertEquals(Integer.valueOf(0), Integer.valueOf(bds.destParent[3]));
+        assertEquals(Integer.valueOf(0), Integer.valueOf(bds.destParent[4]));
     }
 
     @Test
     public void testIntersectIdentification(){
-        final int n = 6;
-        List<List<Edge>> graph = createEmptyGraph(n);
-
-        addUnweightedUndirectedEdge(graph, 0, 1);
-        addUnweightedUndirectedEdge(graph, 2, 1);
-        addUnweightedUndirectedEdge(graph, 1, 3);
-        addUnweightedUndirectedEdge(graph, 3, 4);
-        addUnweightedUndirectedEdge(graph, 4, 5);
-        addUnweightedUndirectedEdge(graph, 4, 6);
+        final int n = 7;
+        Graph g = new Graph();
+        for (int i = 0; i < n; i++) {
+            g.addNode(i);
+        }
+        g.addEdge(0, 1);
+        g.addEdge(2, 1);
+        g.addEdge(1, 3);
+        g.addEdge(3, 4);
+        g.addEdge(4, 5);
+        g.addEdge(4, 6);
 
         int src = 0;
         int dest = 6;
 
-        BidirectionialSearch solver = new BidirectionialSearch(graph, n);
+        BidirectionialSearch solver = new BidirectionialSearch(g, n);
         List<Integer> actualPath = solver.bidirectionalSearch(src, dest);
         if(actualPath == null)
             fail();
@@ -128,16 +130,19 @@ public class BidirectionalSearchTest {
 
     @Test
     public void testGraphWithoutPath(){
-        final int n = 2;
-        List<List<Edge>> graph = createEmptyGraph(n);
+        final int n = 4;
+        Graph g = new Graph();
+        for (int i = 0; i < n; i++) {
+            g.addNode(i);
+        }
 
-        addUnweightedUndirectedEdge(graph, 0, 1);
-        addUnweightedUndirectedEdge(graph, 2, 3);
+        g.addEdge(0, 1);
+        g.addEdge(2, 3);
 
         int src = 0;
         int dest = 3;
 
-        BidirectionialSearch solver = new BidirectionialSearch(graph, n);
+        BidirectionialSearch solver = new BidirectionialSearch(g, n);
         List<Integer> actualPath = solver.bidirectionalSearch(src, dest);
         assertNull(actualPath);
     }
