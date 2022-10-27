67210152c0ae4cbe57f7a52b79ee760ffa1bedf8
SHEELINUNI <shellinunv@gmail.com>
2022-10-27 11:14:14 +1100

    Make some update to the HamiltonianCycle.java

--- src/main/java/com/williamfiset/algorithms/graphtheory/HamiltonianCycle.java
+++ src/main/java/com/williamfiset/algorithms/graphtheory/HamiltonianCycle.java
@@ -6,10 +6,11 @@ import java.util.Arrays;
 
 public class HamiltonianCycle {
         /**elements used to store path and the graph **/
-        private int     V, pathCount;
-        private int[]   path;
-        private int[][] graph;
+        int     V, pathCount;
+        int[]   path;
+        int[][] graph;
 
+        //used for testing
         public void setGraph(int[][] graph1){
             this.graph=graph1;
         }
@@ -27,7 +28,7 @@ public class HamiltonianCycle {
             if(g==null)throw new IllegalArgumentException("Graph cannot be null.");
             V = g.length;
             path = new int[V+1];
-            Arrays.fill(path, -1);
+            Arrays.fill(path, -100);
             graph = g;
             try
             {
@@ -56,22 +57,27 @@ public class HamiltonianCycle {
             /** all vertices selected but last vertex not linked to 0 **/
             if (pathCount == V)
                 return;
-            for (int v = 0; v < V; v++)
+            for (int i = 0; i < V; i++)
             {
                 /** if connected **/
-                if (graph[vertex][v] == 1)
+                if (graph[vertex][i] == 1)
                 {
                     /** add to path **/
-                    path[pathCount++] = v;
+                    path[pathCount++] = i;
                     /** remove connection **/
-                    graph[vertex][v] = 0;
-                    graph[v][vertex] = 0;
+                    graph[vertex][i] = 0;
+                    graph[i][vertex] = 0;
                     /** if vertex not already selected solve recursively **/
-                    if (!isPresent(v))
-                        solve(v);
+                    boolean flag=false;
+                    for (int j= 0; j < pathCount - 1; j++)
+                        if (path[j] == i)
+                            flag= true;
+                    flag= false;
+                    if (!flag)
+                        solve(i);
                     /** restore connection **/
-                    graph[vertex][v] = 1;
-                    graph[v][vertex] = 1;
+                    graph[vertex][i] = 1;
+                    graph[i][vertex] = 1;
                     /** remove path **/
                     path[--pathCount] = -1;
                 }
@@ -79,19 +85,11 @@ public class HamiltonianCycle {
 
         }
 
-        /** function to check if path is already selected **/
-        public boolean isPresent(int v)
-        {
-            for (int i = 0; i < pathCount - 1; i++)
-                if (path[i] == v)
-                    return true;
-            return false;
-        }
 
         /** display solution **/
         public void display()
         {
-            System.out.print("\nPath : ");
+            System.out.print("\nThe Hamiltonian Path is: ");
             for (int i = 0; i <= V; i++)
                 System.out.print(path[i % V] + " ");
             System.out.println();
@@ -105,12 +103,12 @@ public class HamiltonianCycle {
             HamiltonianCycle hc = new HamiltonianCycle();
             /** Accept number of vertices **/
             System.out.println("Enter number of vertices");
-            int V = scan.nextInt();
+            int n = scan.nextInt();
             /** get graph **/
             System.out.println("Enter adjacency matrix");
-            int[][] graph = new int[V][V];
-            for (int i = 0; i < V; i++)
-                for (int j = 0; j < V; j++)
+            int[][] graph = new int[n][n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
                     graph[i][j] = scan.nextInt();
             hc.findHamiltonianCycle(graph);
             scan.close();
