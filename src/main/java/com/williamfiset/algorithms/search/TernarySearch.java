cbf7433a4413b808c463ecd63d95c21f818769f9
Kothwal shaik Sujahid Basha <kt515885@dal.ca>
2023-04-04 15:40:45 -0300

    1. Changed DoubleFunction<Double> to DoubleUnaryOperator in the method signature to avoid unnecessary boxing and unboxing of primitive types.    2. Changed the name of the EPS variable to EPSILON to follow Java naming conventions.-->****Introduce explaining variable refactoring****    3. Changed Double best = null to double best = Double.NaN to avoid null pointer exceptions and improve readability.    4. Added .0 to the division operations to ensure that the results are doubles.    5. Changed the if (res1 > res2) statement to use braces for clarity and consistency.    6. Changed if (best != null && Math.abs(best - mid1) < EPS) to if (&& Math.abs(best - mid1) < EPSILON) to avoid null pointer exceptions and improve readability.

--- src/main/java/com/williamfiset/algorithms/search/TernarySearch.java
+++ src/main/java/com/williamfiset/algorithms/search/TernarySearch.java
@@ -12,24 +12,29 @@
  */
 package com.williamfiset.algorithms.search;
 
-import java.util.function.DoubleFunction;
+import java.util.function.DoubleUnaryOperator;
 
 public class TernarySearch {
 
   // Define a very small epsilon value to compare double values
-  private static final double EPS = 0.000000001;
+  private static final double EPSILON = 1e-9;
 
   // Perform a ternary search on the interval low to high.
   // Remember that your function must be a continuous unimodal
   // function, this means a function which decreases then increases (U shape)
-  public static double ternarySearch(double low, double high, DoubleFunction<Double> function) {
-    Double best = null;
+  public static double ternarySearch(double low, double high, DoubleUnaryOperator function) {
+    double best = Double.NaN;
     while (true) {
-      double mid1 = (2 * low + high) / 3, mid2 = (low + 2 * high) / 3;
-      double res1 = function.apply(mid1), res2 = function.apply(mid2);
-      if (res1 > res2) low = mid1;
-      else high = mid2;
-      if (best != null && Math.abs(best - mid1) < EPS) break;
+      double mid1 = (2 * low + high) / 3.0, mid2 = (low + 2 * high) / 3.0;
+      double res1 = function.applyAsDouble(mid1), res2 = function.applyAsDouble(mid2);
+      if (res1 > res2) {
+        low = mid1;
+      } else {
+        high = mid2;
+      }
+      if (!Double.isNaN(best) && Math.abs(best - mid1) < EPSILON) {
+        break;
+      }
       best = mid1;
     }
     return best;
@@ -39,7 +44,7 @@ public class TernarySearch {
 
     // Search for the lowest point on the function x^2 + 3x + 5
     // using a ternary search on the interval [-100, +100]
-    DoubleFunction<Double> function = (x) -> (x * x + 3 * x + 5);
+    DoubleUnaryOperator function = (x) -> (x * x + 3 * x + 5);
     double root = ternarySearch(-100.0, +100.0, function);
     System.out.printf("%.4f\n", root);
   }
