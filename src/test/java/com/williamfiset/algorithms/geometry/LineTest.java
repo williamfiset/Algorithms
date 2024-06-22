17f8f6b8e01ff3599c0d00cdf14824c9e2aad150
alun04 <alun.arkananta@student.vu.nl>
2024-06-22 14:13:53 +0200

    Changed to assertEquals/Null
    Might solve issues with running on other intelliJ configs.

--- src/test/java/com/williamfiset/algorithms/geometry/LineTest.java
+++ src/test/java/com/williamfiset/algorithms/geometry/LineTest.java
@@ -2,10 +2,12 @@ package com.williamfiset.algorithms.geometry;
 import com.williamfiset.algorithms.CoverageTracker;
 import org.junit.Test;
 
+
 import java.awt.geom.Point2D;
 import java.util.Objects;
 
 import static com.williamfiset.algorithms.geometry.Line.intersection;
+import static org.junit.Assert.*;
 
 public class LineTest {
     @Test
@@ -24,42 +26,42 @@ public class LineTest {
     public void testIntersectionSimple() {
         Line l1 = new Line(0.0,-2.0,0.0,2.0);
         Line l2 = new Line(-2.0,0.0,2.0,0.0);
-        assert(Objects.equals(intersection(l1, l2), new Point2D.Double(0.0, 0.0)));
+        assertEquals(intersection(l1, l2), new Point2D.Double(0.0, 0.0));
     }
 
     @Test
     public void testIntersectionSimple2() {
         Line l2 = new Line(0.0,-2.0,0.0,2.0);
         Line l1 = new Line(-2.0,0.0,2.0,0.0);
-        assert(Objects.equals(intersection(l1, l2), new Point2D.Double(0.0, 0.0)));
+        assertEquals(intersection(l1, l2), new Point2D.Double(0.0, 0.0));
     }
 
     @Test
     public void testIntersectionParallel() {
         Line l1 = new Line(0.0,0.0,1.0,0.0);
         Line l2 = new Line(0.0,1.0,1.0,1.0);
-        assert(Objects.equals(intersection(l1, l2), null));
+        assertNull(intersection(l1, l2));
     }
 
     @Test
     public void testIntersectionHorizontalL1() {
         Line l1 = new Line(0.0,0.0,1.0,0.0);
         Line l2 = new Line(0.0,0.0,1.0,1.0);
-        assert(Objects.equals(intersection(l1, l2), new Point2D.Double(0.0, 0.0)));
+        assertEquals(intersection(l1, l2), new Point2D.Double(0.0, 0.0));
     }
 
     @Test
     public void testIntersectionHorizontalL2() {
         Line l1 = new Line(0.0,0.0,1.0,1.0);
         Line l2 = new Line(0.0,0.0,1.0,0.0);
-        assert(Objects.equals(intersection(l1, l2), new Point2D.Double(0.0, 0.0)));
+        assertEquals(intersection(l1, l2), new Point2D.Double(0.0, 0.0));
     }
 
     @Test
     public void testIntersectionSkewed() {
         Line l1 = new Line(0.0,0.0,2.0,3.0);
         Line l2 = new Line(0.0,0.0,3.0,2.0);
-        assert(Objects.equals(intersection(l1, l2), new Point2D.Double(0.0, 0.0)));
+        assertEquals(intersection(l1, l2), new Point2D.Double(0.0, 0.0));
     }
 }
 
